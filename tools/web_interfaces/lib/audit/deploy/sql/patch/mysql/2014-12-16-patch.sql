3cb8b57f2fbfbef4995e8e758528859dfa9e76ef
Andrea Poli <andreapoli80@gmail.com>
2015-01-20 13:56:51 +0000

    MERGE: Automatic merge of r10518
    The merge fairy automatically merged and checked in the following revision:
    
    Revision 10518 (by apoli)
    svn://openspcoop.org/gov4j/openspcoop2/branches/1.1/1.1.dev => svn://openspcoop.org/gov4j/openspcoop2/branches/2.2/2.2.dev
    
    ________________________________________________________________________________
    
    Patch INDEX DROP
    
    
    ------------------------------------------------------------------------
    
    
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/2.2/2.2.dev@10520 f7a281a6-e321-0410-a5c7-cb86b8957a31

--- tools/web_interfaces/lib/audit/deploy/sql/patch/mysql/2014-12-16-patch.sql
+++ tools/web_interfaces/lib/audit/deploy/sql/patch/mysql/2014-12-16-patch.sql
@@ -1,3 +1,3 @@
-DROP INDEX audit_filter;
+DROP INDEX audit_filter ON audit_operations;
 CREATE INDEX audit_filter_time ON audit_operations (time_request);
 CREATE INDEX audit_filter ON audit_operations (tipo_operazione,tipo,utente,stato);
