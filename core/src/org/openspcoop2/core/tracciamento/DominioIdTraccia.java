b37c5c4eb97d561fb0a8aca2651c5a8bcbb800b6
Andrea Poli <andreapoli80@gmail.com>
2015-03-02 17:13:12 +0000

    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/2.2/2.2.dev@10691 f7a281a6-e321-0410-a5c7-cb86b8957a31

---
+++ core/src/org/openspcoop2/core/tracciamento/DominioIdTraccia.java
@@ -0,0 +1,109 @@
+/*
+ * OpenSPCoop v2 - Customizable SOAP Message Broker 
+ * http://www.openspcoop2.org
+ * 
+ * Copyright (c) 2005-2015 Link.it srl (http://link.it).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.openspcoop2.core.tracciamento;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlType;
+import java.io.Serializable;
+
+
+/** <p>Java class for dominio-id-traccia complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="dominio-id-traccia">
+ * 		&lt;sequence>
+ * 			&lt;element name="identificativo-porta" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="1" maxOccurs="1"/>
+ * 			&lt;element name="soggetto" type="{http://www.openspcoop2.org/core/tracciamento}dominio-soggetto" minOccurs="1" maxOccurs="1"/>
+ * 		&lt;/sequence>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * @version $Rev$, $Date$
+ * 
+ * @author Poli Andrea (poli@link.it)
+ * @author $Author$
+ * */
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "dominio-id-traccia", 
+  propOrder = {
+  	"identificativoPorta",
+  	"soggetto"
+  }
+)
+
+@XmlRootElement(name = "dominio-id-traccia")
+
+public class DominioIdTraccia extends org.openspcoop2.utils.beans.BaseBean implements Serializable , Cloneable {
+  public DominioIdTraccia() {
+  }
+
+  public Long getId() {
+    if(this.id!=null)
+		return this.id;
+	else
+		return new Long(-1);
+  }
+
+  public void setId(Long id) {
+    if(id!=null)
+		this.id=id;
+	else
+		this.id=new Long(-1);
+  }
+
+  public java.lang.String getIdentificativoPorta() {
+    return this.identificativoPorta;
+  }
+
+  public void setIdentificativoPorta(java.lang.String identificativoPorta) {
+    this.identificativoPorta = identificativoPorta;
+  }
+
+  public DominioSoggetto getSoggetto() {
+    return this.soggetto;
+  }
+
+  public void setSoggetto(DominioSoggetto soggetto) {
+    this.soggetto = soggetto;
+  }
+
+  private static final long serialVersionUID = 1L;
+
+  @XmlTransient
+  private Long id;
+
+
+
+  @javax.xml.bind.annotation.XmlSchemaType(name="string")
+  @XmlElement(name="identificativo-porta",required=true,nillable=false)
+  protected java.lang.String identificativoPorta;
+
+  @XmlElement(name="soggetto",required=true,nillable=false)
+  protected DominioSoggetto soggetto;
+
+}
---
+++ core/src/org/openspcoop2/core/tracciamento/DominioIdTracciaSoggetto.java
@@ -0,0 +1,111 @@
+/*
+ * OpenSPCoop v2 - Customizable SOAP Message Broker 
+ * http://www.openspcoop2.org
+ * 
+ * Copyright (c) 2005-2015 Link.it srl (http://link.it).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.openspcoop2.core.tracciamento;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlType;
+import java.io.Serializable;
+
+
+/** <p>Java class for dominio-id-traccia-soggetto complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="dominio-id-traccia-soggetto">
+ * 		<xsd:simpleContent>
+ * 			<xsd:extension base="{http://www.w3.org/2001/XMLSchema}string">
+ * 				&lt;attribute name="tipo" type="{http://www.w3.org/2001/XMLSchema}string" use="required"/>
+ * 			</xsd:extension>
+ * 		</xsd:simpleContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * @version $Rev$, $Date$
+ * 
+ * @author Poli Andrea (poli@link.it)
+ * @author $Author$
+ * */
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "dominio-id-traccia-soggetto")
+
+@XmlRootElement(name = "dominio-id-traccia-soggetto")
+
+public class DominioIdTracciaSoggetto extends org.openspcoop2.utils.beans.BaseBean implements Serializable , Cloneable {
+  public DominioIdTracciaSoggetto() {
+  }
+
+  public Long getId() {
+    if(this.id!=null)
+		return this.id;
+	else
+		return new Long(-1);
+  }
+
+  public void setId(Long id) {
+    if(id!=null)
+		this.id=id;
+	else
+		this.id=new Long(-1);
+  }
+
+  public String getBase() {
+    if(this.base!=null && ("".equals(this.base)==false)){
+		return this.base.trim();
+	}else{
+		return null;
+	}
+
+  }
+
+  public void setBase(String base) {
+    this.base=base;
+  }
+
+  public java.lang.String getTipo() {
+    return this.tipo;
+  }
+
+  public void setTipo(java.lang.String tipo) {
+    this.tipo = tipo;
+  }
+
+  private static final long serialVersionUID = 1L;
+
+  @XmlTransient
+  private Long id;
+
+
+
+  @javax.xml.bind.annotation.XmlSchemaType(name="string")
+  @javax.xml.bind.annotation.XmlValue()
+  public String base;
+
+  @javax.xml.bind.annotation.XmlSchemaType(name="string")
+  @XmlAttribute(name="tipo",required=true)
+  protected java.lang.String tipo;
+
+}
