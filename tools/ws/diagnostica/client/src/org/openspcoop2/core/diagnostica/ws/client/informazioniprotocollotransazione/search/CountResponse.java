5942307c843315b22256dea00254efd452f6ef42
Andrea Poli <andreapoli80@gmail.com>
2015-03-02 17:34:06 +0000

    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/2.2/2.2.dev@10704 f7a281a6-e321-0410-a5c7-cb86b8957a31

---
+++ tools/ws/diagnostica/client/src/org/openspcoop2/core/diagnostica/ws/client/informazioniprotocollotransazione/search/CountResponse.java
@@ -0,0 +1,52 @@
+
+package org.openspcoop2.core.diagnostica.ws.client.informazioniprotocollotransazione.search;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for countResponse complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="countResponse">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="count" type="{http://www.w3.org/2001/XMLSchema}long"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "countResponse", propOrder = {
+    "count"
+})
+public class CountResponse {
+
+    protected long count;
+
+    /**
+     * Gets the value of the count property.
+     * 
+     */
+    public long getCount() {
+        return this.count;
+    }
+
+    /**
+     * Sets the value of the count property.
+     * 
+     */
+    public void setCount(long value) {
+        this.count = value;
+    }
+
+}
---
+++ tools/ws/diagnostica/client/src/org/openspcoop2/core/diagnostica/ws/client/informazioniprotocollotransazione/search/GetResponse.java
@@ -0,0 +1,63 @@
+
+package org.openspcoop2.core.diagnostica.ws.client.informazioniprotocollotransazione.search;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+import org.openspcoop2.core.diagnostica.InformazioniProtocolloTransazione;
+
+
+/**
+ * <p>Java class for getResponse complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="getResponse">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="informazioniProtocolloTransazione" type="{http://www.openspcoop2.org/core/diagnostica}informazioni-protocollo-transazione"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getResponse", propOrder = {
+    "informazioniProtocolloTransazione"
+})
+public class GetResponse {
+
+    @XmlElement(required = true)
+    protected InformazioniProtocolloTransazione informazioniProtocolloTransazione;
+
+    /**
+     * Gets the value of the informazioniProtocolloTransazione property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link InformazioniProtocolloTransazione }
+     *     
+     */
+    public InformazioniProtocolloTransazione getInformazioniProtocolloTransazione() {
+        return this.informazioniProtocolloTransazione;
+    }
+
+    /**
+     * Sets the value of the informazioniProtocolloTransazione property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link InformazioniProtocolloTransazione }
+     *     
+     */
+    public void setInformazioniProtocolloTransazione(InformazioniProtocolloTransazione value) {
+        this.informazioniProtocolloTransazione = value;
+    }
+
+}
