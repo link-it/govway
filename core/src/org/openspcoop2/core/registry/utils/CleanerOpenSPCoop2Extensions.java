ea76498ae56aec81e8acc47bec6fa21604b3dcf5
Andrea Poli <andreapoli80@gmail.com>
2015-03-27 12:06:09 +0000

    MERGE: Automatic merge of r10807
    The merge fairy automatically merged and checked in the following revision:
    
    Revision 10807 (by apoli)
    svn://openspcoop.org/gov4j/openspcoop2/branches/1.1/1.1.dev => svn://openspcoop.org/gov4j/openspcoop2/branches/2.2/2.2.dev
    
    ________________________________________________________________________________
    
    Aggiunte dichiarazioni GPL
    
    
    ------------------------------------------------------------------------
    
    
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/2.2/2.2.dev@10812 f7a281a6-e321-0410-a5c7-cb86b8957a31

--- core/src/org/openspcoop2/core/registry/utils/CleanerOpenSPCoop2Extensions.java
+++ core/src/org/openspcoop2/core/registry/utils/CleanerOpenSPCoop2Extensions.java
@@ -1,3 +1,24 @@
+/*
+ * OpenSPCoop v2 - Customizable SOAP Message Broker 
+ * http://www.openspcoop2.org
+ * 
+ * Copyright (c) 2005-2015 Link.it srl (http://link.it). 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.openspcoop2.core.registry.utils;
 
 import org.openspcoop2.core.registry.AccordoCooperazione;
@@ -14,6 +35,13 @@ import org.openspcoop2.core.registry.ServizioAzione;
 import org.openspcoop2.core.registry.ServizioAzioneFruitore;
 import org.openspcoop2.core.registry.constants.BindingUse;
 
+/**
+ * CleanerOpenSPCoop2Extensions
+ *
+ * @author Poli Andrea (apoli@link.it)
+ * @author $Author$
+ * @version $Rev$, $Date$
+ */
 public class CleanerOpenSPCoop2Extensions {
 
 	public void clean(PortaDominio portaDominio){
--- core/src/org/openspcoop2/pdd/services/connector/ConnectorUtils.java
+++ core/src/org/openspcoop2/pdd/services/connector/ConnectorUtils.java
@@ -1,3 +1,24 @@
+/*
+ * OpenSPCoop v2 - Customizable SOAP Message Broker 
+ * http://www.openspcoop2.org
+ * 
+ * Copyright (c) 2005-2015 Link.it srl (http://link.it). 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.openspcoop2.pdd.services.connector;
 
 import java.io.IOException;
@@ -18,6 +39,13 @@ import org.openspcoop2.protocol.engine.constants.IDService;
 import org.openspcoop2.protocol.sdk.IProtocolFactory;
 import org.openspcoop2.utils.resources.MapReader;
 
+/**
+ * ConnectorUtils
+ *
+ * @author Poli Andrea (apoli@link.it)
+ * @author $Author$
+ * @version $Rev$, $Date$
+ */
 public class ConnectorUtils {
 
 	public static String getMessageHttpMethodNotSupported(MethodType method){
