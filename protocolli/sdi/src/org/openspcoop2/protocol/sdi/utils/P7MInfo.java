1ff75b2bf963531ca0e5f9d515007516ee6c16e0
Andrea Poli <andreapoli80@gmail.com>
2015-04-07 07:14:00 +0000

    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/2.2/2.2.dev@10846 f7a281a6-e321-0410-a5c7-cb86b8957a31

---
+++ protocolli/sdi/src/org/openspcoop2/protocol/sdi/utils/P7MInfo.java
@@ -0,0 +1,62 @@
+package org.openspcoop2.protocol.sdi.utils;
+
+import java.io.ByteArrayInputStream;
+
+import org.apache.log4j.Logger;
+import org.apache.soap.encoding.soapenc.Base64;
+
+public class P7MInfo {
+
+	/*
+	 * Infatti il formato del file rispecchia quanto indicato nella delibera CNIPA numero 45 del 9 Novembre 2009 all’articolo 21, comma 3.
+	 * 
+	 * La delibera cita quanto segue:
+	 * “Per la codifica della busta crittografica deve essere utilizzato il formato
+	 * ASN.1 (ISO/IEC8824) in rappresentazione binaria (ISO/IEC 8825, BER - DER) o
+	 * alfanumerica ottenuta applicando la trasformazione BASE 64 (RFC 1421, RFC
+	 * 2045). La testata e la coda previsti nelle specifiche RFC 1421 e RFC 2045
+	 * possono essere assenti.”
+	 *
+	 * In pratica la fattura P7M puo' essere ricevuta direttamente come
+	 * rappresentazione binaria o codificata in base64.
+	 * 
+	 **/
+	
+	private byte[] xmlDecoded;
+	private boolean base64Encoded = false;
+	
+	public P7MInfo(byte [] fattura, Logger log) throws Throwable{
+		
+		try{
+			org.bouncycastle.cms.CMSSignedData cmsSignedData = new org.bouncycastle.cms.CMSSignedData(new ByteArrayInputStream(fattura));
+			this.xmlDecoded = (byte[]) cmsSignedData.getSignedContent().getContent();
+			
+		}catch(Throwable e){
+			
+			byte[] decoded = null;
+			try{
+				decoded = Base64.decode(new String(fattura));
+			}catch(Throwable eDecode){
+				log.error("DecodificaBase64 non riuscita: "+eDecode.getMessage(), eDecode);
+				throw e; // lancio l'eccezione originale, poiche' piu' interessante. La seconda mi informa solo che non e' una rappresentazione Base64
+			}
+			this.base64Encoded = true;
+			
+			try{
+				org.bouncycastle.cms.CMSSignedData cmsSignedData = new org.bouncycastle.cms.CMSSignedData(new ByteArrayInputStream(decoded));
+				this.xmlDecoded = (byte[]) cmsSignedData.getSignedContent().getContent();				
+			}catch(Throwable eSecond){
+				throw eSecond;
+			}
+		}
+		
+	}
+	
+	public byte[] getXmlDecoded() {
+		return this.xmlDecoded;
+	}
+	public boolean isBase64Encoded() {
+		return this.base64Encoded;
+	}
+	
+}
