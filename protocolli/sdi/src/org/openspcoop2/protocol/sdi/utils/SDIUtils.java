209e1aab9adc8fba49a23e46295e8e4938a2fcbd
Andrea Poli <andreapoli80@gmail.com>
2015-04-07 07:15:28 +0000

    MERGE: Automatic merge of r10842
    The merge fairy automatically merged and checked in the following revision:
    
    Revision 10842 (by apoli)
    svn://openspcoop.org/gov4j/openspcoop2/branches/1.1/1.1.dev => svn://openspcoop.org/gov4j/openspcoop2/branches/2.2/2.2.dev
    
    ________________________________________________________________________________
    
    Aggiunto metodo splitter
    
    
    ------------------------------------------------------------------------
    
    
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/2.2/2.2.dev@10847 f7a281a6-e321-0410-a5c7-cb86b8957a31

--- protocolli/sdi/src/org/openspcoop2/protocol/sdi/utils/SDIUtils.java
+++ protocolli/sdi/src/org/openspcoop2/protocol/sdi/utils/SDIUtils.java
@@ -22,6 +22,7 @@ package org.openspcoop2.protocol.sdi.utils;
 
 import it.gov.fatturapa.sdi.messaggi.v1_0.constants.TipiMessaggi;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
@@ -42,8 +43,15 @@ import org.openspcoop2.protocol.sdk.state.IState;
 import org.openspcoop2.protocol.utils.IDSerialGenerator;
 import org.openspcoop2.protocol.utils.IDSerialGeneratorParameter;
 import org.openspcoop2.protocol.utils.IDSerialGeneratorType;
+import org.openspcoop2.utils.xml.AbstractXMLUtils;
+import org.openspcoop2.utils.xml.DynamicNamespaceContext;
+import org.openspcoop2.utils.xml.XMLUtils;
+import org.openspcoop2.utils.xml.XPathExpressionEngine;
+import org.openspcoop2.utils.xml.XPathNotFoundException;
+import org.openspcoop2.utils.xml.XPathReturnType;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  * SDIUtils
@@ -222,6 +230,69 @@ public class SDIUtils {
 		}
 		
 		return idPaeseIdCodice+"_"+serialGenerator.buildID(serialGeneratorParameter)+ext;
+	}
+	
+	public static List<byte[]> splitLotto(byte[] lotto) throws Exception {
+		
+		List<byte[]> risultato = new ArrayList<byte[]>();
+		
+		AbstractXMLUtils xmlUtils = XMLUtils.getInstance();
+		XPathExpressionEngine xpathEngine = new XPathExpressionEngine();
+		
+		Element lottoElement = null;
+		DynamicNamespaceContext dnc = null;
+		try{
+			lottoElement = xmlUtils.newElement(lotto);
+			dnc = new DynamicNamespaceContext();
+			dnc.findPrefixNamespace(lottoElement);
+		}catch(Exception e){
+			throw new Exception("Lettura lotto di fattura non riuscita: "+e.getMessage(),e);
+		}
+		
+		NodeList nl = null;
+		try{
+			nl = (NodeList) xpathEngine.getMatchPattern(lottoElement, dnc, "//FatturaElettronicaBody", XPathReturnType.NODESET);
+		}catch(XPathNotFoundException notFound){
+			throw new Exception("La fattura non contiene alcun body?: "+notFound.getMessage(),notFound);
+		}catch(Exception e){
+			throw new Exception("Estrazione body dal lotto di fattura non riuscita: "+e.getMessage(),e);
+		}
+		if(nl==null){
+			throw new Exception("La fattura non contiene alcun body? (null)");
+		}
+		if(nl.getLength()<1){
+			throw new Exception("La fattura non contiene alcun body? (size): "+nl.getLength());
+		}
+		
+		if(nl.getLength()==1){
+			// non si tratta di un lotto, ma di una fattura con un body solamente
+			risultato.add(lotto);
+			return risultato;
+		}
+		
+		List<Node> listBody = new ArrayList<Node>();
+		try{
+			for (int i = 0; i < nl.getLength(); i++) {
+				Node n = nl.item(i);
+				listBody.add(n);
+				lottoElement.removeChild(n);
+			}
+		}catch(Exception e){
+			throw new Exception("Elaborazione lotto di fattura (step1) non riuscita: "+e.getMessage(),e);
+		}
+		int i = 0;
+		try{
+			for (; i < listBody.size(); i++) {
+				Node n = listBody.get(i);
+				lottoElement.appendChild(n);
+				risultato.add(xmlUtils.toByteArray(lottoElement));
+				lottoElement.removeChild(n);
+			}
+		}catch(Exception e){
+			throw new Exception("Elaborazione lotto di fattura (step2-"+i+") non riuscita: "+e.getMessage(),e);
+		}
+		
+		return risultato;
 	}
 	
 }
