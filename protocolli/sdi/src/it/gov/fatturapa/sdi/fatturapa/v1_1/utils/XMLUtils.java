0f929fc9baefd2c93f43c16cd7c9a1c6e9eddf40
Andrea Poli <andreapoli80@gmail.com>
2015-04-27 16:39:53 +0000

    MERGE: Automatic merge of r10935
    The merge fairy automatically merged and checked in the following revision:
    
    Revision 10935 (by apoli)
    svn://openspcoop.org/gov4j/openspcoop2/branches/1.1/1.1.dev => svn://openspcoop.org/gov4j/openspcoop2/branches/2.2/2.2.dev
    
    ________________________________________________________________________________
    
    GPL
    
    
    ------------------------------------------------------------------------
    
    
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/2.2/2.2.dev@10937 f7a281a6-e321-0410-a5c7-cb86b8957a31

--- protocolli/sdi/src/it/gov/fatturapa/sdi/fatturapa/v1_1/utils/XMLUtils.java
+++ protocolli/sdi/src/it/gov/fatturapa/sdi/fatturapa/v1_1/utils/XMLUtils.java
@@ -1,9 +1,38 @@
+/*
+ * OpenSPCoop v2 - Customizable SOAP Message Broker 
+ * http://www.openspcoop2.org
+ * 
+ * Copyright (c) 2005-2015 Link.it srl (http://link.it).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package it.gov.fatturapa.sdi.fatturapa.v1_1.utils;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
+
+/**
+ * XMLUtils
+ * 
+ * @author Andrea Poli (apoli@link.it)
+ * @author $Author$
+ * @version $Rev$, $Date$
+ */
 public class XMLUtils {
 
 	public static boolean isFattura(byte [] doc){
--- protocolli/sdi/src/org/openspcoop2/protocol/sdi/utils/P7MInfo.java
+++ protocolli/sdi/src/org/openspcoop2/protocol/sdi/utils/P7MInfo.java
@@ -1,3 +1,24 @@
+/*
+ * OpenSPCoop v2 - Customizable SOAP Message Broker 
+ * http://www.openspcoop2.org
+ * 
+ * Copyright (c) 2005-2015 Link.it srl (http://link.it).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.openspcoop2.protocol.sdi.utils;
 
 import java.io.ByteArrayInputStream;
@@ -5,6 +26,13 @@ import java.io.ByteArrayInputStream;
 import org.apache.log4j.Logger;
 import org.apache.soap.encoding.soapenc.Base64;
 
+/**
+ * P7MInfo
+ * 
+ * @author Andrea Poli (apoli@link.it)
+ * @author $Author$
+ * @version $Rev$, $Date$
+ */
 public class P7MInfo {
 
 	/*
