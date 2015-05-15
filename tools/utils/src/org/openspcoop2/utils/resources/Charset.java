1a5ac8915813bbd5cccc38371b6527e78dd83fe9
Andrea Poli <andreapoli80@gmail.com>
2015-05-15 13:16:20 +0000

    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/2.2/2.2.dev@10962 f7a281a6-e321-0410-a5c7-cb86b8957a31

---
+++ tools/utils/src/org/openspcoop2/utils/resources/Charset.java
@@ -0,0 +1,174 @@
+/*
+ * OpenSPCoop v2 - Customizable SOAP Message Broker 
+ * http://www.openspcoop2.org
+ * 
+ * Copyright (c) 2005-2015 Link.it srl (http://link.it).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.openspcoop2.utils.resources;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.apache.commons.lang.CharEncoding;
+import org.openspcoop2.generic_project.beans.IEnumeration;
+
+/**
+* CharsetEncoding
+*
+* @author Andrea Poli <apoli@link.it>
+* @author $Author$
+* @version $Rev$, $Date$
+*/
+public enum Charset implements IEnumeration , Serializable , Cloneable {
+
+	ISO_8859_1 (CharEncoding.ISO_8859_1),
+	UTF_8 (CharEncoding.UTF_8),
+	UTF_16 (CharEncoding.UTF_16),
+	UTF_16BE (CharEncoding.UTF_16BE),
+	UTF_16LE (CharEncoding.UTF_16LE),
+	US_ASCII (CharEncoding.US_ASCII);
+	
+	
+	/** Value */
+	private String value;
+	@Override
+	public String getValue()
+	{
+		return this.value;
+	}
+
+
+	/** Official Constructor */
+	Charset(String value)
+	{
+		this.value = value;
+	}
+
+
+	
+	@Override
+	public String toString(){
+		return this.value;
+	}
+	public boolean equals(Charset object){
+		if(object==null)
+			return false;
+		if(object.getValue()==null)
+			return false;
+		return object.getValue().equals(this.getValue());	
+	}
+	public boolean equals(String object){
+		if(object==null)
+			return false;
+		return object.equals(this.getValue());	
+	}
+	
+		
+	
+	/** compatibility with the generated bean (reflection) */
+	public boolean equals(Object object,List<String> fieldsNotCheck){
+		if( !(object instanceof Charset) ){
+			throw new RuntimeException("Wrong type: "+object.getClass().getName());
+		}
+		return this.equals(((Charset)object));
+	}
+	public String toString(boolean reportHTML){
+		return toString();
+	}
+  	public String toString(boolean reportHTML,List<String> fieldsNotIncluded){
+  		return toString();
+  	}
+  	public String diff(Object object,StringBuffer bf,boolean reportHTML){
+		return bf.toString();
+	}
+	public String diff(Object object,StringBuffer bf,boolean reportHTML,List<String> fieldsNotIncluded){
+		return bf.toString();
+	}
+	
+	
+	/** Utilities */
+	
+	public static String[] toArray(){
+		String[] res = new String[values().length];
+		int i=0;
+		for (Charset tmp : values()) {
+			res[i]=tmp.getValue();
+			i++;
+		}
+		return res;
+	}	
+	public static String[] toStringArray(){
+		String[] res = new String[values().length];
+		int i=0;
+		for (Charset tmp : values()) {
+			res[i]=tmp.toString();
+			i++;
+		}
+		return res;
+	}
+	public static String[] toEnumNameArray(){
+		String[] res = new String[values().length];
+		int i=0;
+		for (Charset tmp : values()) {
+			res[i]=tmp.name();
+			i++;
+		}
+		return res;
+	}
+	
+	public static boolean contains(String value){
+		return toEnumConstant(value)!=null;
+	}
+	
+	public static Charset toEnumConstant(String value){
+		Charset res = null;
+		if(Charset.ISO_8859_1.getValue().equals(value)){
+			res = Charset.ISO_8859_1;
+		}else if(Charset.US_ASCII.getValue().equals(value)){
+			res = Charset.US_ASCII;
+		}else if(Charset.UTF_8.getValue().equals(value)){
+			res = Charset.UTF_8;
+		}else if(Charset.UTF_16.getValue().equals(value)){
+			res = Charset.UTF_16;
+		}else if(Charset.UTF_16BE.getValue().equals(value)){
+			res = Charset.UTF_16BE;
+		}else if(Charset.UTF_16LE.getValue().equals(value)){
+			res = Charset.UTF_16LE;
+		}
+		return res;
+	}
+	
+	public static IEnumeration toEnumConstantFromString(String value){
+		Charset res = null;
+		if(Charset.ISO_8859_1.toString().equals(value)){
+			res = Charset.ISO_8859_1;
+		}else if(Charset.US_ASCII.toString().equals(value)){
+			res = Charset.US_ASCII;
+		}else if(Charset.UTF_8.toString().equals(value)){
+			res = Charset.UTF_8;
+		}else if(Charset.UTF_16.toString().equals(value)){
+			res = Charset.UTF_16;
+		}else if(Charset.UTF_16BE.toString().equals(value)){
+			res = Charset.UTF_16BE;
+		}else if(Charset.UTF_16LE.toString().equals(value)){
+			res = Charset.UTF_16LE;
+		}
+		return res;
+	}
+	
+}
---
+++ tools/utils/src/org/openspcoop2/utils/resources/RFC2047Utilities.java
@@ -0,0 +1,98 @@
+/*
+ * OpenSPCoop v2 - Customizable SOAP Message Broker 
+ * http://www.openspcoop2.org
+ * 
+ * Copyright (c) 2005-2015 Link.it srl (http://link.it).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.openspcoop2.utils.resources;
+
+import javax.mail.internet.MimeUtility;
+
+import org.openspcoop2.utils.UtilsException;
+
+/**
+* RFC2047Utilities
+*
+* @author Andrea Poli <apoli@link.it>
+* @author $Author$
+* @version $Rev$, $Date$
+*/
+
+public class RFC2047Utilities {
+
+	public static void main(String[] args) throws Exception {
+		
+		// Test String con caratteri appartenenti all'ISO ISO_8859_1
+		String string_iso_8859_1 = "für psychisch";
+		String string_us_ascii = "fur psychisch";
+		
+		System.out.println("IS ["+string_iso_8859_1+"] in CHARSET ISO_8859_1 ?: "+isAllCharactersInCharset(string_iso_8859_1, Charset.ISO_8859_1));
+		System.out.println("IS ["+string_iso_8859_1+"] in CHARSET US_ASCII ?: "+isAllCharactersInCharset(string_iso_8859_1, Charset.US_ASCII));
+		System.out.println("IS ["+string_us_ascii+"] in CHARSET ISO_8859_1 ?: "+isAllCharactersInCharset(string_us_ascii, Charset.ISO_8859_1));
+		System.out.println("IS ["+string_us_ascii+"] in CHARSET US_ASCII ?: "+isAllCharactersInCharset(string_us_ascii, Charset.US_ASCII));
+		
+		System.out.println("\n*** RFC2047 ISO_8859_1 B ***");
+		String var_RFC2047_ISO_8859_1_B = encode(string_iso_8859_1, Charset.ISO_8859_1, RFC2047Encoding.B);
+		System.out.println("["+string_iso_8859_1+"] encoded in RFC2047("+Charset.ISO_8859_1+","+RFC2047Encoding.B+"): "+var_RFC2047_ISO_8859_1_B);
+		System.out.println("Decode: "+decode(var_RFC2047_ISO_8859_1_B));
+		
+		System.out.println("\n*** RFC2047 ISO_8859_1 Q ***");
+		String var_RFC2047_ISO_8859_1_Q = encode(string_iso_8859_1, Charset.ISO_8859_1, RFC2047Encoding.Q);
+		System.out.println("["+string_iso_8859_1+"] encoded in RFC2047("+Charset.ISO_8859_1+","+RFC2047Encoding.Q+"): "+var_RFC2047_ISO_8859_1_Q);
+		System.out.println("Decode: "+decode(var_RFC2047_ISO_8859_1_Q));
+		
+		System.out.println("\n*** RFC2047 US_ASCII B ***");
+		String var_RFC2047_US_ASCII_B = encode(string_iso_8859_1, Charset.US_ASCII, RFC2047Encoding.B);
+		System.out.println("["+string_iso_8859_1+"] encoded in RFC2047("+Charset.US_ASCII+","+RFC2047Encoding.B+"): "+var_RFC2047_US_ASCII_B);
+		System.out.println("Decode: "+decode(var_RFC2047_US_ASCII_B));
+		
+		System.out.println("\n*** RFC2047 US_ASCII Q ***");
+		String var_RFC2047_US_ASCII_Q = encode(string_iso_8859_1, Charset.US_ASCII, RFC2047Encoding.Q);
+		System.out.println("["+string_iso_8859_1+"] encoded in RFC2047("+Charset.US_ASCII+","+RFC2047Encoding.Q+"): "+var_RFC2047_US_ASCII_Q);
+		System.out.println("Decode: "+decode(var_RFC2047_US_ASCII_Q));
+	}
+	
+	public static String encode(String value,Charset charset,RFC2047Encoding encoding) throws UtilsException{
+		try{
+			return MimeUtility.encodeWord(value,charset.getValue(),encoding.name());
+		}catch(Exception e){
+			throw new UtilsException(e.getMessage(),e);
+		}
+	}
+	
+	public static String decode(String value) throws UtilsException{
+		try{
+			return MimeUtility.decodeWord(value);
+		}catch(Exception e){
+			throw new UtilsException(e.getMessage(),e);
+		}
+	}
+	
+	public static boolean isAllCharactersInCharset(String value,Charset charset) throws UtilsException{
+		try{
+			java.nio.charset.CharsetEncoder encoder = 
+					java.nio.charset.Charset.forName(charset.getValue()).newEncoder();
+			return encoder.canEncode(value);
+		}catch(Exception e){
+			throw new UtilsException(e.getMessage(),e);
+		}
+	}
+	
+}
+
+
