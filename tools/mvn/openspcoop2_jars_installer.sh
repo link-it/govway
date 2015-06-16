347728fa721033b9b31b5d7f4ac9b025ea32668f
Giuliano Pintori <pintori@link.it>
2015-06-16 08:34:15 +0000

    Sistemato lo script maven per la condivisione dei jar della libreria sul repository maven openspcoop.
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/2.2/2.2.dev@11013 f7a281a6-e321-0410-a5c7-cb86b8957a31

--- tools/mvn/openspcoop2_jars_installer.sh
+++ tools/mvn/openspcoop2_jars_installer.sh
@@ -7,6 +7,8 @@ then
 fi
 
 OPENSPCOOP_VERSION="$(svn info | grep URL | cut -d '/' -f 7)"
+#utilizzare per le versioni di svn >= 1.8
+#OPENSPCOOP_VERSION="$(svn info | grep 'URL: svn' | cut -d '/' -f 7)"
 
 URL_REPOSITORY="scp://maven.openspcoop.org/var/maven2/repositories/public/"
 
@@ -110,6 +112,8 @@ echo "Position: [${PWD}]"
 
 echo "Installazione JAR Openspcoop2 WebGenericProjectImpl nel repository Link.it in corso..."
 
+cp pom.xml ../web_generic_project/impl
+
 pushd ../web_generic_project/impl
 
 echo "Generazione dei JAR in corso..."
@@ -139,35 +143,36 @@ mvn deploy:deploy-file -DrepositoryId=link-repository -DpomFile=jsf1/dist/$WEB_G
 
 echo "Deploy del JAR $WEB_GENERIC_PROJECT_IMPL_JSF1_JAR completato."
 
-echo "Deploy del JAR $WEB_GENERIC_PROJECT_IMPL_JSF2_JAR in corso..."
+#echo "Deploy del JAR $WEB_GENERIC_PROJECT_IMPL_JSF2_JAR in corso..."
 
-echo "<project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
-        xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd\">
-        <artifactId>$WEB_GENERIC_PROJECT_IMPL_JSF2_JAR</artifactId>
-        <groupId>org.openspcoop2</groupId>
-        <version>$MVN_VERSION</version>
-        <modelVersion>4.0.0</modelVersion>
-        <name>Openspcoop2 Web Generic Project</name>
-        <dependencies> 
-                <dependency>
-                        <groupId>org.openspcoop2</groupId>
-                        <artifactId>$WEB_GENERIC_PROJECT_JAR</artifactId>
-                        <version>$MVN_VERSION</version>
-                </dependency>
-        </dependencies>
-</project>" > "jsf2/dist/${WEB_GENERIC_PROJECT_IMPL_JSF2_JAR}_${MVN_VERSION}.pom"
+#echo "<project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
+#        xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd\">
+#        <artifactId>$WEB_GENERIC_PROJECT_IMPL_JSF2_JAR</artifactId>
+#        <groupId>org.openspcoop2</groupId>
+#        <version>$MVN_VERSION</version>
+#        <modelVersion>4.0.0</modelVersion>
+#        <name>Openspcoop2 Web Generic Project</name>
+#        <dependencies> 
+#                <dependency>
+#                        <groupId>org.openspcoop2</groupId>
+#                        <artifactId>$WEB_GENERIC_PROJECT_JAR</artifactId>
+#                        <version>$MVN_VERSION</version>
+#                </dependency>
+#        </dependencies>
+#</project>" > "jsf2/dist/${WEB_GENERIC_PROJECT_IMPL_JSF2_JAR}_${MVN_VERSION}.pom"
 
-mvn deploy:deploy-file -DrepositoryId=link-repository -DpomFile=jsf2/dist/$WEB_GENERIC_PROJECT_IMPL_JSF2_JAR"_"$MVN_VERSION.pom -Dfile=jsf2/dist/$WEB_GENERIC_PROJECT_IMPL_JSF2_JAR"_"$OPENSPCOOP_BUILD_VERSION.jar -Durl=$URL_REPOSITORY
+#mvn deploy:deploy-file -DrepositoryId=link-repository -DpomFile=jsf2/dist/$WEB_GENERIC_PROJECT_IMPL_JSF2_JAR"_"$MVN_VERSION.pom -Dfile=jsf2/dist/$WEB_GENERIC_PROJECT_IMPL_JSF2_JAR"_"$OPENSPCOOP_BUILD_VERSION.jar -Durl=$URL_REPOSITORY
 
-echo "Deploy del JAR $WEB_GENERIC_PROJECT_IMPL_JSF2_JAR completato."
+#echo "Deploy del JAR $WEB_GENERIC_PROJECT_IMPL_JSF2_JAR completato."
 
 rm -f "jsf1/dist/${WEB_GENERIC_PROJECT_IMPL_JSF1_JAR}_${MVN_VERSION}.pom"
-rm -f "jsf1/dist/${WEB_GENERIC_PROJECT_IMPL_JSF2_JAR}_${MVN_VERSION}.pom"
+#rm -f "jsf2/dist/${WEB_GENERIC_PROJECT_IMPL_JSF2_JAR}_${MVN_VERSION}.pom"
 
 popd 
 
 echo "Installazione JAR Openspcoop2 WebGenericProjectImpl nel repository Link.it completata."
 
+rm -f ../web_generic_project/impl/pom.xml
 rm -f ../web_generic_project/pom.xml
 
 
