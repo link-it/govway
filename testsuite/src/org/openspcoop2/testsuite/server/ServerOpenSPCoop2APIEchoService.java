3e0b639af83ee183eb44bcc4596be973eecf31de
Andrea Poli <andreapoli80@gmail.com>
2015-06-29 13:03:34 +0000

    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/2.2/2.2.dev@11043 f7a281a6-e321-0410-a5c7-cb86b8957a31

---
+++ testsuite/src/org/openspcoop2/testsuite/server/ServerOpenSPCoop2APIEchoService.java
@@ -0,0 +1,121 @@
+/*
+ * OpenSPCoop v2 - Customizable SOAP Message Broker 
+ * http://www.openspcoop2.org
+ * 
+ * Copyright (c) 2005-2015 Link.it srl (http://link.it). 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+
+package org.openspcoop2.testsuite.server;
+
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+/**
+ * Server EchoService
+ * 
+ * @author Poli Andrea (apoli@link.it)
+ * @author $Author: bussu $
+ * @version $Rev: 10664 $, $Date: 2015-02-23 18:10:28 +0100 (Mon, 23 Feb 2015) $
+ */
+
+@SuppressWarnings("serial")
+public class ServerOpenSPCoop2APIEchoService extends ServerCore{
+
+
+	/**
+	 * 
+	 */
+	public ServerOpenSPCoop2APIEchoService() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	@Override
+	public void init() {}
+
+	
+	private void dispatch(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException{
+		
+		Properties pHeaderRisposta = this.testsuiteProperties.getHeaderRisposta();
+		if(pHeaderRisposta==null){
+			pHeaderRisposta = new Properties();
+		}
+		pHeaderRisposta.put(this.testsuiteProperties.getHeaderRispostaServletName(), "APIEchoService");
+		
+		ServerOpenSPCoop2EchoService echo = new ServerOpenSPCoop2EchoService();
+		echo.engine(request, response, pHeaderRisposta);
+	}
+	
+	
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		this.dispatch(req, resp);
+	}
+
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		this.dispatch(req, resp);
+	}
+
+
+	@Override
+	protected void doHead(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		this.dispatch(req, resp);
+	}
+
+
+	@Override
+	protected void doOptions(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		this.dispatch(req, resp);
+	}
+
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		this.dispatch(req, resp);
+	}
+
+
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		this.dispatch(req, resp);
+	}
+
+
+	@Override
+	protected void doTrace(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		this.dispatch(req, resp);
+	}
+}
