0e94413bececd20a08053ce3bc5797110fc87802
Andrea Poli <andreapoli80@gmail.com>
2015-09-22 08:09:50 +0000

    JavaDoc
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/2.3/2.3.dev@11206 f7a281a6-e321-0410-a5c7-cb86b8957a31

--- tools/web_generic_project/impl/jsf2/src/org/openspcoop2/generic_project/web/impl/jsf2/listener/KeepAliveListener.java
+++ tools/web_generic_project/impl/jsf2/src/org/openspcoop2/generic_project/web/impl/jsf2/listener/KeepAliveListener.java
@@ -1,3 +1,24 @@
+/*
+ * OpenSPCoop v2 - Customizable SOAP Message Broker 
+ * http://www.openspcoop2.org
+ * 
+ * Copyright (c) 2005-2015 Link.it srl (http://link.it).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.openspcoop2.generic_project.web.impl.jsf2.listener;
 
 import java.util.HashMap;
@@ -12,6 +33,15 @@ import javax.faces.event.PhaseListener;
 import org.apache.log4j.Logger;
 import org.openspcoop2.generic_project.web.logging.LoggerManager;
 
+/***
+ * 
+ * KeepAliveListener
+ * 
+ * 
+ * @author Pintori Giuliano (pintori@link.it)
+ * @author $Author$
+ * @version $Rev$, $Date$ 
+ */
 public class KeepAliveListener  implements PhaseListener {
 
     private static final long serialVersionUID = -3689434283608539768L;
