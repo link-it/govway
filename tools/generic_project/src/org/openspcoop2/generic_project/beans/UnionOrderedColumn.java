0c4f3d13ff776be478736255ebd8e504528024f6
Andrea Poli <andreapoli80@gmail.com>
2015-09-23 14:13:44 +0000

    MERGE: Automatic merge of r11216
    The merge fairy automatically merged and checked in the following revision:
    
    Revision 11216 (by apoli)
    svn://openspcoop.org/gov4j/openspcoop2/branches/2.2/2.2.dev => svn://openspcoop.org/gov4j/openspcoop2/branches/2.3/2.3.dev
    
    ________________________________________________________________________________
    
    src/org/openspcoop2/generic_project/beans/UnionOrderedColumn.java
    
    
    ------------------------------------------------------------------------
    
    
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/2.3/2.3.dev@11223 f7a281a6-e321-0410-a5c7-cb86b8957a31

---
+++ tools/generic_project/src/org/openspcoop2/generic_project/beans/UnionOrderedColumn.java
@@ -0,0 +1,54 @@
+/*
+ * OpenSPCoop v2 - Customizable SOAP Message Broker 
+ * http://www.openspcoop2.org
+ * 
+ * Copyright (c) 2005-2015 Link.it srl (http://link.it).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.openspcoop2.generic_project.beans;
+
+
+import org.openspcoop2.generic_project.expression.SortOrder;
+
+/**
+ * UnionOrderedColumn
+ * 
+ * @author Poli Andrea (apoli@link.it)
+ * @author $Author$
+ * @version $Rev$, $Date$
+ */
+public class UnionOrderedColumn {
+
+	private String alias;
+	private SortOrder sortOrder;
+	
+	public UnionOrderedColumn(String alias, SortOrder sortOrder){
+		this.alias = alias;
+		this.sortOrder = sortOrder;
+	}
+	
+	public UnionOrderedColumn(String alias){
+		this.alias = alias;
+	}
+	
+	public String getAlias() {
+		return this.alias;
+	}
+	public SortOrder getSortOrder() {
+		return this.sortOrder;
+	}
+	
+}
