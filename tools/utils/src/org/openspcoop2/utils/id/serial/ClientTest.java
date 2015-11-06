c419f9b0ef723d152551c771de2aa8c2d867e91c
Andrea Poli <andreapoli80@gmail.com>
2015-11-06 16:41:20 +0000

    Aggiunto javaDoc in tools/utils/src/org/openspcoop2/utils/id/serial/ClientTest.java
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/2.3/2.3.dev@11269 f7a281a6-e321-0410-a5c7-cb86b8957a31

--- tools/utils/src/org/openspcoop2/utils/id/serial/ClientTest.java
+++ tools/utils/src/org/openspcoop2/utils/id/serial/ClientTest.java
@@ -1,3 +1,24 @@
+/*
+ * OpenSPCoop v2 - Customizable SOAP Message Broker 
+ * http://www.openspcoop2.org
+ * 
+ * Copyright (c) 2005-2015 Link.it srl (http://link.it). 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.openspcoop2.utils.id.serial;
 
 import java.sql.Connection;
@@ -12,6 +33,13 @@ import org.openspcoop2.utils.TipiDatabase;
 import org.openspcoop2.utils.UtilsException;
 import org.openspcoop2.utils.regexp.RegularExpressionEngine;
 
+/**
+ * ClientTest
+ *
+ * @author Andrea Poli (apoli@link.it)
+ * @author $Author$
+ * @version $Rev$, $Date$
+ */
 public class ClientTest {
 
 	public static void main(String[] args) throws Exception {
