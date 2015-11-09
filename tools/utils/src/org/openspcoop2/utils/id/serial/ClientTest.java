5e46656d9dee3098fe8243f236a807c5d3cea52e
Andrea Poli <andreapoli80@gmail.com>
2015-11-09 09:39:48 +0000

    Modificata classe di test utils/src/org/openspcoop2/utils/id/serial/ClientTest.java per utilizzare threads in parallelo
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/2.3/2.3.dev@11270 f7a281a6-e321-0410-a5c7-cb86b8957a31

--- tools/utils/src/org/openspcoop2/utils/id/serial/ClientTest.java
+++ tools/utils/src/org/openspcoop2/utils/id/serial/ClientTest.java
@@ -26,7 +26,11 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.apache.log4j.Logger;
 import org.openspcoop2.utils.TipiDatabase;
@@ -115,34 +119,24 @@ public class ClientTest {
 			}
 		}
 
+		
+		boolean DEBUG = false;
 
 		Class.forName(driver).newInstance();
 		Connection con = null;
+		List<Connection> conThreads = new ArrayList<Connection>();
+		int numThreads = 10;
 		Statement stmtDelete = null;
 		PreparedStatement stmtInsert = null;
 		try{
 			con = DriverManager.getConnection(url, userName, password);
 
 
-			String delete = "delete from id_messaggio_relativo";
-			stmtDelete = con.createStatement();
-			stmtDelete.execute(delete);
-			stmtDelete.close();
+			clear(con, stmtDelete);
 			
-			delete = "delete from id_messaggio";
-			stmtDelete = con.createStatement();
-			stmtDelete.execute(delete);
-			stmtDelete.close();
-			
-			delete = "delete from id_messaggio_prg";
-			stmtDelete = con.createStatement();
-			stmtDelete.execute(delete);
-			stmtDelete.close();
-			
-			delete = "delete from id_messaggio_relativo_prg";
-			stmtDelete = con.createStatement();
-			stmtDelete.execute(delete);
-			stmtDelete.close();
+			for (int i = 0; i < numThreads; i++) {
+				conThreads.add(DriverManager.getConnection(url, userName, password));
+			}
 			
 			
 			Logger log = Logger.getLogger(ClientTest.class);
@@ -163,10 +157,13 @@ public class ClientTest {
 			serialGeneratorParameter.setWrap(false);			
 			System.out.println("\n\n==========================================");
 			System.out.println("Test 1. Progressivo numerico");
-			test(serialGenerator, serialGeneratorParameter, con, log, true);
+			test(serialGenerator, serialGeneratorParameter, conThreads, log, true, DEBUG);
 			
 			
 			/** TEST N.2 PROGRESSIVO con MAX */
+			
+			ClientTestThread.reset();
+			clear(con, stmtDelete);
 			
 			serialGeneratorParameter.setTipo(IDSerialGeneratorType.NUMERIC);
 			serialGeneratorParameter.setWrap(false);
@@ -175,7 +172,7 @@ public class ClientTest {
 			System.out.println("Test 2. Progressivo numerico con max 10");
 			boolean foundError = false;
 			try{
-				test(serialGenerator, serialGeneratorParameter, con, log, true);
+				test(serialGenerator, serialGeneratorParameter, conThreads, log, true, DEBUG);
 			}catch(Exception e){
 				foundError = true;
 				System.out.println("Errore Atteso: "+e.getMessage());
@@ -186,17 +183,23 @@ public class ClientTest {
 			
 			/** TEST N.3 PROGRESSIVO con MAX e Wrap */
 			
+			ClientTestThread.reset();
+			clear(con, stmtDelete);
+			
 			serialGeneratorParameter.setTipo(IDSerialGeneratorType.NUMERIC);
 			serialGeneratorParameter.setWrap(true);
 			serialGeneratorParameter.setMaxValue(10l);
 			System.out.println("\n\n==========================================");
 			System.out.println("Test 3. Progressivo numerico con max 10 e wrap");
-			test(serialGenerator, serialGeneratorParameter, con, log, true);
+			test(serialGenerator, serialGeneratorParameter, conThreads, log, true, DEBUG);
 			
 			
 			
 			
 			/** TEST N.4 PROGRESSIVO (InfoAssociata) */
+			
+			ClientTestThread.reset();
+			clear(con, stmtDelete);
 			
 			serialGeneratorParameter.setTipo(IDSerialGeneratorType.NUMERIC);
 			serialGeneratorParameter.setWrap(false);
@@ -204,11 +207,14 @@ public class ClientTest {
 			serialGeneratorParameter.setInformazioneAssociataAlProgressivo("Associata");
 			System.out.println("\n\n==========================================");
 			System.out.println("Test 4. Progressivo numerico (InfoAssociata)");
-			test(serialGenerator, serialGeneratorParameter, con, log, true);
+			test(serialGenerator, serialGeneratorParameter, conThreads, log, true, DEBUG);
 			
 			
 			/** TEST N.5 PROGRESSIVO con MAX (InfoAssociata) */
 			
+			ClientTestThread.reset();
+			clear(con, stmtDelete);
+
 			serialGeneratorParameter.setTipo(IDSerialGeneratorType.NUMERIC);
 			serialGeneratorParameter.setWrap(false);
 			serialGeneratorParameter.setMaxValue(10l);
@@ -217,7 +223,7 @@ public class ClientTest {
 			System.out.println("Test 5. Progressivo numerico con max 10 (InfoAssociata)");
 			foundError = false;
 			try{
-				test(serialGenerator, serialGeneratorParameter, con, log, true);
+				test(serialGenerator, serialGeneratorParameter, conThreads, log, true, DEBUG);
 			}catch(Exception e){
 				foundError = true;
 				System.out.println("Errore Atteso: "+e.getMessage());
@@ -228,13 +234,17 @@ public class ClientTest {
 			
 			/** TEST N.6 PROGRESSIVO con MAX e Wrap (InfoAssociata)  */
 			
+			ClientTestThread.reset();
+			clear(con, stmtDelete);
+
+			
 			serialGeneratorParameter.setTipo(IDSerialGeneratorType.NUMERIC);
 			serialGeneratorParameter.setWrap(true);
 			serialGeneratorParameter.setMaxValue(10l);
 			serialGeneratorParameter.setInformazioneAssociataAlProgressivo("Associata");
 			System.out.println("\n\n==========================================");
 			System.out.println("Test 6. Progressivo numerico con max 10 e wrap");
-			test(serialGenerator, serialGeneratorParameter, con, log, true);
+			test(serialGenerator, serialGeneratorParameter, conThreads, log, true, DEBUG);
 			
 			
 			
@@ -242,15 +252,18 @@ public class ClientTest {
 			
 			/** TEST N.7 PROGRESSIVO con MAX */
 			
+			ClientTestThread.reset();
+			clear(con, stmtDelete);
+	
 			serialGeneratorParameter.setTipo(IDSerialGeneratorType.ALFANUMERICO);
 			serialGeneratorParameter.setWrap(false);
 			serialGeneratorParameter.setSize(1);
 			serialGeneratorParameter.setInformazioneAssociataAlProgressivo(null); // annullo il precedente assegnamento
 			System.out.println("\n\n==========================================");
-			System.out.println("Test 8. Progressivo alfanumerico con size 2");
+			System.out.println("Test 8. Progressivo alfanumerico con size 1");
 			foundError = false;
 			try{
-				test(serialGenerator, serialGeneratorParameter, con, log, false);
+				test(serialGenerator, serialGeneratorParameter, conThreads, log, false, DEBUG);
 			}catch(Exception e){
 				foundError = true;
 				System.out.println("Errore Atteso: "+e.getMessage());
@@ -261,26 +274,32 @@ public class ClientTest {
 			
 			/** TEST N.8 PROGRESSIVO con MAX e Wrap */
 			
+			ClientTestThread.reset();
+			clear(con, stmtDelete);
+			
 			serialGeneratorParameter.setTipo(IDSerialGeneratorType.ALFANUMERICO);
 			serialGeneratorParameter.setWrap(true);
 			serialGeneratorParameter.setSize(1);
 			System.out.println("\n\n==========================================");
-			System.out.println("Test 9. Progressivo numerico con size 2 e wrap");
-			test(serialGenerator, serialGeneratorParameter, con, log, false);
+			System.out.println("Test 9. Progressivo numerico con size 1 e wrap");
+			test(serialGenerator, serialGeneratorParameter, conThreads, log, false, DEBUG);
 			
 			
 			
 			/** TEST N.9 PROGRESSIVO con MAX (InfoAssociata) */
 			
+			ClientTestThread.reset();
+			clear(con, stmtDelete);
+		
 			serialGeneratorParameter.setTipo(IDSerialGeneratorType.ALFANUMERICO);
 			serialGeneratorParameter.setWrap(false);
 			serialGeneratorParameter.setSize(1);
 			serialGeneratorParameter.setInformazioneAssociataAlProgressivo("InfoAssociata");
 			System.out.println("\n\n==========================================");
-			System.out.println("Test 9. Progressivo alfanumerico con size 2 (InfoAssociata)");
+			System.out.println("Test 9. Progressivo alfanumerico con size 1 (InfoAssociata)");
 			foundError = false;
 			try{
-				test(serialGenerator, serialGeneratorParameter, con, log, false);
+				test(serialGenerator, serialGeneratorParameter, conThreads, log, false, DEBUG);
 			}catch(Exception e){
 				foundError = true;
 				System.out.println("Errore Atteso: "+e.getMessage());
@@ -291,13 +310,16 @@ public class ClientTest {
 			
 			/** TEST N.10 PROGRESSIVO con MAX e Wrap (InfoAssociata) */
 			
+			ClientTestThread.reset();
+			clear(con, stmtDelete);
+		
 			serialGeneratorParameter.setTipo(IDSerialGeneratorType.ALFANUMERICO);
 			serialGeneratorParameter.setWrap(true);
 			serialGeneratorParameter.setSize(1);
 			serialGeneratorParameter.setInformazioneAssociataAlProgressivo("InfoAssociata");
 			System.out.println("\n\n==========================================");
-			System.out.println("Test 10. Progressivo numerico con size 2 e wrap (InfoAssociata)");
-			test(serialGenerator, serialGeneratorParameter, con, log, false);
+			System.out.println("Test 10. Progressivo numerico con size 1 e wrap (InfoAssociata)");
+			test(serialGenerator, serialGeneratorParameter, conThreads, log, false, DEBUG);
 
 			
 			
@@ -311,51 +333,223 @@ public class ClientTest {
 			try{
 				con.close();
 			}catch(Exception eClose){}
+			for (int i = 0; i < numThreads; i++) {
+				try{
+					conThreads.get(i).close();
+				}catch(Exception eClose){}
+			}
 		}
 
 
 	}
 
-
+	public static void clear(Connection con, Statement stmtDelete) throws Exception{
+		String delete = "delete from id_messaggio_relativo";
+		stmtDelete = con.createStatement();
+		stmtDelete.execute(delete);
+		stmtDelete.close();
+		
+		delete = "delete from id_messaggio";
+		stmtDelete = con.createStatement();
+		stmtDelete.execute(delete);
+		stmtDelete.close();
+		
+		delete = "delete from id_messaggio_prg";
+		stmtDelete = con.createStatement();
+		stmtDelete.execute(delete);
+		stmtDelete.close();
+		
+		delete = "delete from id_messaggio_relativo_prg";
+		stmtDelete = con.createStatement();
+		stmtDelete.execute(delete);
+		stmtDelete.close();
+	}
 	
 	public static void test(IDSerialGenerator serialGenerator, IDSerialGeneratorParameter param,
-			Connection con, Logger log, boolean isNumber) throws Exception{
-		List<String> check = new ArrayList<String>();
+			List<Connection> conThreads, Logger log, boolean isNumber, boolean debug) throws Exception{
 		
-		String v = null;
-		int i = 0;
-		boolean rilevatiValoriDuplicati = false;
+		ExecutorService threadsPool = Executors.newFixedThreadPool(conThreads.size());
+		Hashtable<String, ClientTestThread> threads = new Hashtable<String, ClientTestThread>();
+		
+		boolean error = false;
+		try {
+		
+			for (int i = 0; i < conThreads.size(); i++) {
+				
+				ClientTestThread c = new ClientTestThread(serialGenerator, param, conThreads.get(i), log, isNumber,i,debug);
+				threadsPool.execute(c);
+				if(debug)
+					System.out.println("Lanciato thread "+i);
+				threads.put("Thread-"+i, c);
+				
+			}
+		
+			boolean terminated = false;
+			while(terminated == false){
+				if(debug)
+					System.out.println("Attendo terminazione ...");
+				boolean tmpTerminated = true;
+				for (int i = 0; i < conThreads.size(); i++) {
+					
+					ClientTestThread c = threads.get("Thread-"+i);
+					if(c.isError()){
+						error = true;
+					}
+					if(c.isFinished()==false){
+						tmpTerminated = false;
+						break;
+					}
+				}
+				if(tmpTerminated==false){
+					try{
+						Thread.sleep(250);
+					}catch(Exception e){}
+				}
+				else{
+					terminated = true;
+				}
+			}
+			
+		} finally{
+			log.info("Shutdown pool ...");
+			threadsPool.shutdown(); 
+			log.info("Shutdown pool ok");
+		}
+		
+		
+		for (int i = 0; i < conThreads.size(); i++) {
+			ClientTestThread c = threads.get("Thread-"+i);
+			if(c.getValoriGenerati().size()>0)
+				System.out.println("[Thread-"+i+"] ha generato "+c.getValoriGenerati().size()+" id. Ultimo: "+c.getValoriGenerati().get(c.getValoriGenerati().size()-1));
+			else
+				System.out.println("[Thread-"+i+"] non ha generato id.");
+			if(debug){
+				System.out.println("[Thread-"+i+"] ids generati: "+c.getValoriGenerati().toString());
+			}
+		}
+		System.out.println("Duplicati rilevati: "+ClientTestThread.isValoriDuplicati());
+		System.out.println("Identificativi distinti complessivi generati: "+ClientTestThread.count());
+		Collections.sort(ClientTestThread.getValoriDistintiGenerati());
+		System.out.println("Identificativi: "+ClientTestThread.getValoriDistintiGenerati());
+		
+		if(error){
+			throw new Exception("Error occurs in threads");
+		}
+	}
+	
+}
+
+
+class ClientTestThread implements Runnable{
+
+	private static List<String> check = new ArrayList<String>();
+	private static boolean valoriDuplicati = false;
+	
+	private static synchronized void put(String v,boolean rilevaValoriDuplicati) throws Exception{
+		if(!check.contains(v)){
+			check.add(v);
+		}else{
+			if(rilevaValoriDuplicati){
+				throw new Exception("Valore ["+v+"] gia generato");
+			}
+			else{
+				valoriDuplicati = true;
+			}
+		}
+	}
+	
+	public static List<String> getValoriDistintiGenerati() {
+		return check;
+	}
+
+	public static void reset(){
+		check.clear();
+		valoriDuplicati = false;
+	}
+	
+	public static int count(){
+		return check.size();
+	}
+	
+	public static boolean isValoriDuplicati() {
+		return valoriDuplicati;
+	}
+	
+	private IDSerialGenerator serialGenerator;
+	private IDSerialGeneratorParameter param;
+	private Connection con;
+	private Logger log;
+	private boolean isNumber;
+	private List<String> valoriGenerati;
+	private int index;
+	private boolean debug;
+	public List<String> getValoriGenerati() {
+		return this.valoriGenerati;
+	}
+
+	private boolean finished = false;
+	private boolean error = false;
+	
+	public boolean isError() {
+		return this.error;
+	}
+
+	public boolean isFinished() {
+		return this.finished;
+	}
+
+	public ClientTestThread(IDSerialGenerator serialGenerator, IDSerialGeneratorParameter param, Connection con, Logger log,
+			boolean isNumber, int index, boolean debug){
+		this.serialGenerator = serialGenerator;
+		this.param = param;
+		this.con = con;
+		this.log = log;
+		this.isNumber = isNumber;
+		this.valoriGenerati = new ArrayList<String>();
+		this.index = index;
+		this.debug = debug;
+	}
+	
+	@Override
+	public void run() {
+		
 		try{
+		
+			String v = null;
+			int i = 0;
+			boolean rilevaValoriDuplicati = false;
+			if(this.param.isWrap()==false){
+				rilevaValoriDuplicati = true;
+			}
+	
 			int limit = 100;
 			for (; i < limit; i++) {
+				if(this.debug){
+					if(i%10==0){
+						System.out.println("[Thread-"+this.index+"] Generati "+i+" ids");
+					}
+				}
 				v = null;
-				if(isNumber){
-					long l = serialGenerator.buildIDAsNumber(param, con, log);
+				if(this.isNumber){
+					long l = this.serialGenerator.buildIDAsNumber(this.param, this.con, this.log);
 					v = l + "";
 				}
 				else{
-					v = serialGenerator.buildID(param, con, log);
+					v = this.serialGenerator.buildID(this.param, this.con, this.log);
 					if(!RegularExpressionEngine.isMatch((v+""),"^[a-zA-Z0-9]*$")){
 						throw new UtilsException("Deve essere fornito [a-zA-Z0-9] trovato ["+v+"]");
 					}
 				}
-				if(!check.contains(v)){
-					check.add(v);
-				}else{
-					if(param.isWrap()){
-						rilevatiValoriDuplicati = true;
-					}else{
-						throw new Exception("Valore ["+v+"] gia generato");
-					}
-				}
-				if(i<20){
-					System.out.println("VALORE ["+i+"]: "+v);
-				}else if(i%50 == 0 )
-					System.out.println("VALORE ["+i+"]: "+v);
+				this.valoriGenerati.add(v);
+				put(v, rilevaValoriDuplicati);
 			}
-		}finally{
-			System.out.println("VALORE LAST ["+i+"]: "+v);
-			System.out.println("Duplicati: "+rilevatiValoriDuplicati);
+			
+		}catch(Exception e){
+			this.error = true;
+			throw new RuntimeException(e.getMessage(),e);
+		}
+		finally{
+			this.finished = true;
 		}
 	}
 	
