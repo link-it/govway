30a70f7833c48241115be70a4e5f284a6c8e518f
Andrea Poli <andreapoli80@gmail.com>
2016-04-21 15:43:17 +0000

    MERGE: Automatic merge of r11887
    The merge fairy automatically merged and checked in the following revision:
    
    Revision 11887 (by apoli)
    svn://openspcoop.org/gov4j/openspcoop2/branches/2.2/2.2.dev => svn://openspcoop.org/gov4j/openspcoop2/branches/2.3/2.3.dev
    
    ________________________________________________________________________________
    
    Set Property SVN
    
    
    ------------------------------------------------------------------------
    
    
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/2.3/2.3.dev@11890 f7a281a6-e321-0410-a5c7-cb86b8957a31

--- tools/utils/src/org/openspcoop2/utils/NameValue.java
+++ tools/utils/src/org/openspcoop2/utils/NameValue.java
@@ -25,8 +25,8 @@ package org.openspcoop2.utils;
 * NameValue
 *
 * @author Andrea Poli <apoli@link.it>
- * @author $Author: apoli $
- * @version $Rev: 11423 $, $Date: 2016-01-25 16:58:18 +0100 (Mon, 25 Jan 2016) $
+ * @author $Author$
+ * @version $Rev$, $Date$
 */
 public class NameValue {
 
