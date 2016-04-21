6ceb53529935630a0ed7f1f4b74f193c1211b44a
Andrea Poli <andreapoli80@gmail.com>
2016-04-21 15:43:03 +0000

    MERGE: Automatic merge of r11886
    The merge fairy automatically merged and checked in the following revision:
    
    Revision 11886 (by apoli)
    svn://openspcoop.org/gov4j/openspcoop2/branches/2.2/2.2.dev => svn://openspcoop.org/gov4j/openspcoop2/branches/2.3/2.3.dev
    
    ________________________________________________________________________________
    
    Aggiunta dichiarazione GPL
    
    
    ------------------------------------------------------------------------
    
    
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/2.3/2.3.dev@11889 f7a281a6-e321-0410-a5c7-cb86b8957a31

--- core/src/org/openspcoop2/pdd/services/DumpRaw.java
+++ core/src/org/openspcoop2/pdd/services/DumpRaw.java
@@ -1,3 +1,24 @@
+/*
+ * OpenSPCoop v2 - Customizable SOAP Message Broker 
+ * http://www.openspcoop2.org
+ * 
+ * Copyright (c) 2005-2016 Link.it srl (http://link.it). 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.openspcoop2.pdd.services;
 
 import java.security.cert.X509Certificate;
@@ -15,6 +36,13 @@ import org.openspcoop2.protocol.engine.constants.IDService;
 import org.openspcoop2.utils.Identity;
 import org.openspcoop2.utils.io.notifier.NotifierInputStreamParams;
 
+/**
+ * DumpRaw
+ * 
+ * @author Poli Andrea (apoli@link.it)
+ * @author $Author$
+ * @version $Rev$, $Date$
+ */
 public class DumpRaw {
 
 	private static final String format = "yyyy-MM-dd_HH:mm:ss.SSS";
