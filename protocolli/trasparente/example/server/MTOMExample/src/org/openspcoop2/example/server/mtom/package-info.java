a7db192f16904b58998bd1f18dc02541b13fa7e0
Andrea Poli <andreapoli80@gmail.com>
2017-03-07 14:16:39 +0000

    MERGE: Automatic merge of r12754
    The merge fairy automatically merged and checked in the following revision:
    
    Revision 12754 (by apoli)
    svn://openspcoop.org/gov4j/openspcoop2/branches/2.3/2.3.dev => svn://openspcoop.org/gov4j/openspcoop2/branches/3.0/3.0.dev
    
    ________________________________________________________________________________
    
    Corretta dichiarazione GPL
    
    
    ------------------------------------------------------------------------
    
    
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/3.0/3.0.dev@12755 f7a281a6-e321-0410-a5c7-cb86b8957a31

--- protocolli/trasparente/example/server/MTOMExample/src/org/openspcoop2/example/server/mtom/package-info.java
+++ protocolli/trasparente/example/server/MTOMExample/src/org/openspcoop2/example/server/mtom/package-info.java
@@ -1,5 +1,5 @@
 /*
- * OpenSPCoop v2 - Customizable SOAP Message Broker 
+ * OpenSPCoop - Customizable API Gateway 
  * http://www.openspcoop2.org
  * 
  * Copyright (c) 2005-2017 Link.it srl (http://link.it).
