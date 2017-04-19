0dd91067142ed9839781558500421f18bbc92ab5
Andrea Poli <andreapoli80@gmail.com>
2017-04-19 13:31:52 +0000

    MERGE: Automatic merge of r12902
    The merge fairy automatically merged and checked in the following revision:
    
    Revision 12902 (by apoli)
    svn://openspcoop.org/gov4j/openspcoop2/branches/2.3/2.3.dev => svn://openspcoop.org/gov4j/openspcoop2/branches/3.0/3.0.dev
    
    ________________________________________________________________________________
    
    JavaDoc IFilter.java
    
    
    ------------------------------------------------------------------------
    
    
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/3.0/3.0.dev@12907 f7a281a6-e321-0410-a5c7-cb86b8957a31

--- tools/utils/src/org/openspcoop2/utils/resources/IFilter.java
+++ tools/utils/src/org/openspcoop2/utils/resources/IFilter.java
@@ -1,3 +1,23 @@
+/*
+ * OpenSPCoop - Customizable API Gateway 
+ * http://www.openspcoop2.org
+ * 
+ * Copyright (c) 2005-2017 Link.it srl (http://link.it).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.openspcoop2.utils.resources;
 
 import java.io.IOException;
@@ -7,6 +27,13 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+/**
+* CharsetEncoding
+*
+* @author Giuliano Pintori <pintori@link.it>
+* @author $Author$
+* @version $Rev$, $Date$
+*/
 public interface IFilter {
 
 	public void init(Object ... params) throws IOException, ServletException;
