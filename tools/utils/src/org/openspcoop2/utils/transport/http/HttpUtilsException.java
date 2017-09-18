77d00ea8a6c164e50ad8a3c2088eefeb1b93f962
Andrea Poli <andreapoli80@gmail.com>
2017-09-18 10:06:40 +0000

    MERGE 13287 add HttpUtilsException.java
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/3.0/3.0.dev@13291 f7a281a6-e321-0410-a5c7-cb86b8957a31

---
+++ tools/utils/src/org/openspcoop2/utils/transport/http/HttpUtilsException.java
@@ -0,0 +1,68 @@
+/*
+ * OpenSPCoop - Customizable API Gateway 
+ * http://www.openspcoop2.org
+ * 
+ * Copyright (c) 2005-2017 Link.it srl (http://link.it). 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+
+package org.openspcoop2.utils.transport.http;
+
+/**	
+ * CHttpUtilsException
+ *
+ * @author Poli Andrea (apoli@link.it)
+ * @author $Author: apoli $
+ * @version $Rev: 13288 $, $Date: 2017-09-18 11:57:27 +0200 (Mon, 18 Sep 2017) $
+ */
+
+
+public class HttpUtilsException extends Exception {
+
+	private int returnCode;
+	
+	public int getReturnCode() {
+		return this.returnCode;
+	}
+	public void setReturnCode(int returnCode) {
+		this.returnCode = returnCode;
+	}
+	public HttpUtilsException(int returnCode, String message, Throwable cause)
+	{
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+		this.returnCode = returnCode;
+	}
+	public HttpUtilsException(int returnCode, Throwable cause)
+	{
+		super(cause);
+		// TODO Auto-generated constructor stub
+		this.returnCode = returnCode;
+	}
+	/**
+	 * serialVersionUID
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public HttpUtilsException() {
+		super();
+	}
+	public HttpUtilsException(int returnCode, String msg) {
+		super(msg);
+		this.returnCode = returnCode;
+	}
+}
