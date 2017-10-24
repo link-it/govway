ea2ea7b5a8a854e5bdc28d5c9b91fa45a4717002
Andrea Poli <andreapoli80@gmail.com>
2017-10-24 14:12:49 +0000

    Java Doc
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/3.0/3.0.dev@13373 f7a281a6-e321-0410-a5c7-cb86b8957a31

--- tools/utils/src/org/openspcoop2/utils/json/validation/NetworkNTJsonschemaValidator.java
+++ tools/utils/src/org/openspcoop2/utils/json/validation/NetworkNTJsonschemaValidator.java
@@ -1,3 +1,23 @@
+/*
+ * OpenSPCoop - Customizable API Gateway 
+ * http://www.openspcoop2.org
+ * 
+ * Copyright (c) 2005-2017 Link.it srl (http://link.it). 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.openspcoop2.utils.json.validation;
 
 import java.util.Set;
@@ -14,6 +34,13 @@ import com.networknt.schema.ValidationMessage;
 
 import org.openspcoop2.utils.json.ValidationResponse.ESITO;
 
+/**
+ * NetworkNTJsonschemaValidator
+ *
+ * @author Giovanni Bussu (bussu@link.it)
+ * @author $Author$
+ * @version $Rev$, $Date$
+ */
 public class NetworkNTJsonschemaValidator implements IJsonSchemaValidator {
 
 	
--- tools/utils/src/org/openspcoop2/utils/swagger/validator/SwaggerApiValidatorConfig.java
+++ tools/utils/src/org/openspcoop2/utils/swagger/validator/SwaggerApiValidatorConfig.java
@@ -1,5 +1,21 @@
-/**
+/*
+ * OpenSPCoop - Customizable API Gateway 
+ * http://www.openspcoop2.org
  * 
+ * Copyright (c) 2005-2017 Link.it srl (http://link.it). 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 package org.openspcoop2.utils.swagger.validator;
 
@@ -7,10 +23,12 @@ import org.openspcoop2.utils.json.JsonValidatorAPI;
 import org.openspcoop2.utils.rest.ApiValidatorConfig;
 
 /**
- * @author Bussu Giovanni (bussu@link.it)
- * @author  $Author$
- * @version $ Rev: 12563 $, $Date$
+ * SwaggerApiValidatorConfig
  * 
+ * @author Bussu Giovanni (bussu@link.it)
+ * @author $Author$
+ * @version $Rev$, $Date$
+ *
  */
 public class SwaggerApiValidatorConfig extends ApiValidatorConfig {
 
