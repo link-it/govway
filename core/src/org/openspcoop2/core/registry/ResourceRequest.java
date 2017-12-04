41ff97d718bc2833b5ba1e24651dd76adf7ea244
Andrea Poli <andreapoli80@gmail.com>
2017-12-04 08:23:15 +0000

    Eliminato warning
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/3.0/3.0.dev@13482 f7a281a6-e321-0410-a5c7-cb86b8957a31

--- core/src/org/openspcoop2/core/registry/ResourceRequest.java
+++ core/src/org/openspcoop2/core/registry/ResourceRequest.java
@@ -21,7 +21,7 @@ package org.openspcoop2.core.registry;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
