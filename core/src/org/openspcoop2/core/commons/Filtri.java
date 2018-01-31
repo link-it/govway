e2f05639f26e698217ef0171c4739de6f7433693
Andrea Poli <andreapoli80@gmail.com>
2018-01-31 15:33:34 +0000

    Filtri
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/3.0/3.0.dev@13590 f7a281a6-e321-0410-a5c7-cb86b8957a31

---
+++ core/src/org/openspcoop2/core/commons/Filtri.java
@@ -0,0 +1,42 @@
+/*
+ * OpenSPCoop - Customizable API Gateway
+ * http://www.openspcoop2.org
+ * 
+ * Copyright (c) 2005-2018 Link.it srl (http://link.it). 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+
+
+package org.openspcoop2.core.commons;
+
+/**
+ * Filtri
+ * 
+ * @author Stefano Corallo - corallo@link.it
+ * @author $Author: apoli $
+ * @version $Rev: 13574 $, $Date: 2018-01-26 12:24:34 +0100 (Fri, 26 Jan 2018) $
+ */
+
+public final class Filtri
+{
+	public final static String FILTRO_PROTOCOLLO = "filtroProtocollo";
+	public final static String FILTRO_DOMINIO = "filtroDominio";
+	public final static String FILTRO_SERVICE_BINDING = "filtroServiceBinding";
+	
+	public final static String FILTRO_UTENTE = "filtroUtente";
+	
+}
