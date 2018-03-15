5e7e34a7bce5c72f7251fcbc6d16add92afbf071
Andrea Poli <andreapoli80@gmail.com>
2018-03-15 16:46:35 +0000

    Commit file RestCollaborationProfile
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/3.0/3.0.dev@13736 f7a281a6-e321-0410-a5c7-cb86b8957a31

---
+++ core/src/org/openspcoop2/protocol/manifest/RestCollaborationProfile.java
@@ -0,0 +1,75 @@
+/*
+ * OpenSPCoop - Customizable API Gateway 
+ * http://www.openspcoop2.org
+ * 
+ * Copyright (c) 2005-2018 Link.it srl (http://link.it).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.openspcoop2.protocol.manifest;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.io.Serializable;
+
+
+/** <p>Java class for RestCollaborationProfile complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="RestCollaborationProfile">
+ * 		&lt;attribute name="oneway" type="{http://www.w3.org/2001/XMLSchema}boolean" use="optional" default="false"/>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * @version $Rev$, $Date$
+ * 
+ * @author Poli Andrea (poli@link.it)
+ * @author $Author$
+ * */
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "RestCollaborationProfile")
+
+@XmlRootElement(name = "RestCollaborationProfile")
+
+public class RestCollaborationProfile extends org.openspcoop2.utils.beans.BaseBean implements Serializable , Cloneable {
+  public RestCollaborationProfile() {
+  }
+
+  public boolean isOneway() {
+    return this.oneway;
+  }
+
+  public boolean getOneway() {
+    return this.oneway;
+  }
+
+  public void setOneway(boolean oneway) {
+    this.oneway = oneway;
+  }
+
+  private static final long serialVersionUID = 1L;
+
+
+
+  @javax.xml.bind.annotation.XmlSchemaType(name="boolean")
+  @XmlAttribute(name="oneway",required=false)
+  protected boolean oneway = false;
+
+}
