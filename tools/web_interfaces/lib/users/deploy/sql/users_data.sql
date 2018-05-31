7370e66812969567b8dac4801039781758d7ecd7
Andrea Poli <andreapoli80@gmail.com>
2018-05-31 11:02:13 +0000

    Completato setup per utenti
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/3.0/3.0.dev@14062 f7a281a6-e321-0410-a5c7-cb86b8957a31

--- tools/web_interfaces/lib/users/deploy/sql/users_data.sql
+++ tools/web_interfaces/lib/users/deploy/sql/users_data.sql
@@ -1,3 +1,2 @@
 -- users
-INSERT INTO users (login, password, tipo_interfaccia, permessi) VALUES ('super',	'$1$.bquKJDS$yZ4EYC3354HqEjmSRL/sR0','STANDARD','U');
-INSERT INTO users (login, password, tipo_interfaccia, permessi) VALUES ('amministratore',	'$1$.bquKJDS$yZ4EYC3354HqEjmSRL/sR0','STANDARD','SDCMA');
+INSERT INTO users (login, password, tipo_interfaccia, permessi) VALUES ('amministratore',	'$1$.bquKJDS$yZ4EYC3354HqEjmSRL/sR0','STANDARD','SDCMAU');
