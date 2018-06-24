858faa4324e4c77eba6a131eb6bd5885f5fb472a
Andrea Poli <andreapoli80@gmail.com>
2018-06-24 11:23:44 +0000

    Message GPL
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/3.0/3.0.dev@14184 f7a281a6-e321-0410-a5c7-cb86b8957a31

--- core/src/org/openspcoop2/message/utils/WWWAuthenticateGenerator.java
+++ core/src/org/openspcoop2/message/utils/WWWAuthenticateGenerator.java
@@ -1,3 +1,25 @@
+/*
+ * GovWay - A customizable API Gateway 
+ * http://www.govway.org
+ * 
+ * from the Link.it OpenSPCoop project codebase
+ * 
+ * Copyright (c) 2005-2018 Link.it srl (http://link.it).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.openspcoop2.message.utils;
 
 import java.util.Properties;
@@ -9,6 +31,13 @@ import org.openspcoop2.message.constants.MessageRole;
 import org.openspcoop2.message.constants.MessageType;
 import org.openspcoop2.utils.transport.http.HttpConstants;
 
+/**     
+ * WWWAuthenticateGenerator
+ *
+ * @author Poli Andrea (poli@link.it)
+ * @author $Author$
+ * @version $Rev$, $Date$
+ */
 public class WWWAuthenticateGenerator {
 	
 	public static OpenSPCoop2Message buildErrorMessage(WWWAuthenticateErrorCode errorCode, String realm, boolean genericError, String error, String ... scope) {
