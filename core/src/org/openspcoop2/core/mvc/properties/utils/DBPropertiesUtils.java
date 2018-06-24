8a172fd77e93541279781fcbcc92e2075529111d
Andrea Poli <andreapoli80@gmail.com>
2018-06-24 11:40:24 +0000

    GPL core
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/3.0/3.0.dev@14185 f7a281a6-e321-0410-a5c7-cb86b8957a31

--- core/src/org/openspcoop2/core/mvc/properties/utils/DBPropertiesUtils.java
+++ core/src/org/openspcoop2/core/mvc/properties/utils/DBPropertiesUtils.java
@@ -1,3 +1,25 @@
+/*
+ * GovWay - A customizable API Gateway 
+ * http://www.govway.org
+ * 
+ * from the Link.it OpenSPCoop project codebase
+ * 
+ * Copyright (c) 2005-2018 Link.it srl (http://link.it).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.openspcoop2.core.mvc.properties.utils;
 
 import java.sql.Connection;
@@ -13,6 +35,13 @@ import org.openspcoop2.core.commons.CoreException;
 import org.openspcoop2.utils.sql.ISQLQueryObject;
 import org.openspcoop2.utils.sql.SQLObjectFactory;
 
+/**     
+ * DBPropertiesUtils
+ *
+ * @author Poli Andrea (poli@link.it)
+ * @author $Author$
+ * @version $Rev$, $Date$
+ */
 public class DBPropertiesUtils {
 
 	public static Map<String, String> readProperties(Connection con, String tipoDB, String nomeTabella, String nomeColonnaKey, String nomeColonnaValue, String nomeColonnaParent, Long idParent) throws CoreException{
