466a205e1ae8045baa6f75973ae7a52fffeae0d0
Andrea Poli <andreapoli80@gmail.com>
2018-06-25 09:32:29 +0000

    GPL Loader
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/3.0/3.0.dev@14210 f7a281a6-e321-0410-a5c7-cb86b8957a31

--- tools/web_interfaces/loader/src/org/openspcoop2/web/loader/servlet/login/MessagePage.java
+++ tools/web_interfaces/loader/src/org/openspcoop2/web/loader/servlet/login/MessagePage.java
@@ -1,3 +1,25 @@
+/*
+ * GovWay - A customizable API Gateway 
+ * http://www.govway.org
+ *
+ * from the Link.it OpenSPCoop project codebase
+ * 
+ * Copyright (c) 2005-2018 Link.it srl (http://link.it).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.openspcoop2.web.loader.servlet.login;
 
 import java.util.ArrayList;
@@ -22,6 +44,15 @@ import org.openspcoop2.web.loader.servlet.LoaderHelper;
 import org.openspcoop2.web.loader.core.Costanti;
 import org.openspcoop2.web.loader.core.LoaderCore;
 
+/****
+ * 
+ * MessagePage
+ * 
+ * @author Pintori Giuliano (pintori@link.it)
+ * @author $Author$
+ * @version $Rev$, $Date$
+ *
+ */
 public class MessagePage extends Action {
 
 	@Override
