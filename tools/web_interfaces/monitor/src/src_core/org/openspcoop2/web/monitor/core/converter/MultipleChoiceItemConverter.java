afff59bcf02fadaf242c86c3a8a98a58c9e33997
Andrea Poli <andreapoli80@gmail.com>
2018-06-25 09:37:47 +0000

    GPL
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/3.0/3.0.dev@14211 f7a281a6-e321-0410-a5c7-cb86b8957a31

--- tools/web_interfaces/monitor/src/src_core/org/openspcoop2/web/monitor/core/converter/MultipleChoiceItemConverter.java
+++ tools/web_interfaces/monitor/src/src_core/org/openspcoop2/web/monitor/core/converter/MultipleChoiceItemConverter.java
@@ -1,13 +1,14 @@
 /*
- * OpenSPCoop v2 - Customizable SOAP Message Broker 
- * http://www.openspcoop2.org
+ * GovWay - A customizable API Gateway 
+ * http://www.govway.org
  * 
- * Copyright (c) 2005-2016 Link.it srl (http://link.it).
+ * from the Link.it OpenSPCoop project codebase
+ * 
+ * Copyright (c) 2005-2018 Link.it srl (http://link.it).
  * 
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
--- tools/web_interfaces/monitor/src/src_core/org/openspcoop2/web/monitor/core/core/ReportType.java
+++ tools/web_interfaces/monitor/src/src_core/org/openspcoop2/web/monitor/core/core/ReportType.java
@@ -1,3 +1,24 @@
+/*
+ * GovWay - A customizable API Gateway 
+ * http://www.govway.org
+ * 
+ * from the Link.it OpenSPCoop project codebase
+ * 
+ * Copyright (c) 2005-2018 Link.it srl (http://link.it).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package org.openspcoop2.web.monitor.core.core;
 
 public enum ReportType {
