0a26c70e3c53d8cd19b75ccbebc249bdc78ee441
Giuliano Pintori <pintori@link.it>
2018-06-26 16:45:56 +0000

    gestione eccezioni.
    git-svn-id: svn://openspcoop.org/gov4j/openspcoop2/branches/3.0/3.0.dev@14223 f7a281a6-e321-0410-a5c7-cb86b8957a31

--- core/src/org/openspcoop2/core/transazioni/utils/CredenzialiMittenteUtils.java
+++ core/src/org/openspcoop2/core/transazioni/utils/CredenzialiMittenteUtils.java
@@ -28,9 +28,14 @@ import java.util.Hashtable;
 import org.openspcoop2.core.config.constants.TipoAutenticazione;
 import org.openspcoop2.core.transazioni.CredenzialeMittente;
 import org.openspcoop2.core.transazioni.dao.ICredenzialeMittenteService;
+import org.openspcoop2.generic_project.exception.ExpressionException;
+import org.openspcoop2.generic_project.exception.ExpressionNotImplementedException;
+import org.openspcoop2.generic_project.exception.NotImplementedException;
+import org.openspcoop2.generic_project.exception.ServiceException;
 import org.openspcoop2.generic_project.expression.IPaginatedExpression;
 import org.openspcoop2.generic_project.expression.LikeMode;
 import org.openspcoop2.utils.Utilities;
+import org.openspcoop2.utils.UtilsException;
 
 /**     
  * CredenzialiMittenteUtils
@@ -43,7 +48,7 @@ public class CredenzialiMittenteUtils {
 
 	public static IPaginatedExpression createCredenzialeMittentePaginatedExpression(ICredenzialeMittenteService credenzialeMittentiService,
     		TipoCredenzialeMittente tipoCredenziale, String tipoAutenticazione, String credential,
-    		boolean ricercaEsatta, boolean caseSensitive) throws Exception {
+    		boolean ricercaEsatta, boolean caseSensitive) throws ServiceException, NotImplementedException, ExpressionNotImplementedException, ExpressionException, UtilsException {
     	IPaginatedExpression pagEpression = credenzialeMittentiService.newPaginatedExpression();
 		pagEpression.and();
 		if(TipoCredenzialeMittente.trasporto.equals(tipoCredenziale)) {
@@ -52,7 +57,7 @@ public class CredenzialiMittenteUtils {
 		else {
 			pagEpression.equals(CredenzialeMittente.model().TIPO, tipoCredenziale.name());
 		}
-		if(TipoCredenzialeMittente.trasporto.equals(tipoCredenziale) && TipoAutenticazione.SSL.getValue().equalsIgnoreCase(tipoAutenticazione)) {
+		if(TipoCredenzialeMittente.trasporto.equals(tipoCredenziale) && TipoAutenticazione.SSL.getValue().equalsIgnoreCase(tipoAutenticazione) && ricercaEsatta) {
 			Hashtable<String, String> hashSubject = Utilities.getSubjectIntoHashtable(credential);			
 			Enumeration<String> keys = hashSubject.keys();			
 			while(keys.hasMoreElements()){				
