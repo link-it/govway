f24bb9d6a7019ad6258f00dbb48647ab8bdd9215
Andrea Poli <andreapoli80@gmail.com>
2019-11-15 11:59:11 +0100

    Aggiunto GPL nel file tools/batch/statistiche/src/org/openspcoop2/core/statistiche/batch/GeneratorProperties.java

--- tools/batch/statistiche/src/org/openspcoop2/core/statistiche/batch/GeneratorProperties.java
+++ tools/batch/statistiche/src/org/openspcoop2/core/statistiche/batch/GeneratorProperties.java
@@ -1,3 +1,26 @@
+/*
+ * GovWay - A customizable API Gateway 
+ * http://www.govway.org
+ *
+ * from the Link.it OpenSPCoop project codebase
+ * 
+ * Copyright (c) 2005-2019 Link.it srl (http://link.it).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
 package org.openspcoop2.core.statistiche.batch;
 
 import java.io.File;
@@ -6,6 +29,13 @@ import java.util.Properties;
 
 import org.openspcoop2.monitor.engine.statistic.StatisticsForceIndexConfig;
 
+/**
+* GeneratorProperties
+*
+* @author Poli Andrea (apoli@link.it)
+* @author $Author$
+* @version $Rev$, $Date$
+*/
 public class GeneratorProperties {
 	
 	private static GeneratorProperties staticInstance = null;
