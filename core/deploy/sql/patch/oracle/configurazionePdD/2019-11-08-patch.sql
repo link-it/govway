6ff8f2806ad867d273b644c295c0240b4fc63516
Andrea Poli <andreapoli80@gmail.com>
2019-11-15 16:48:28 +0100

    Aggiornato resources/scenari/scenari.zip per adeguamento a versione 3.2.1

--- core/deploy/sql/patch/oracle/configurazionePdD/2019-11-08-patch.sql
+++ core/deploy/sql/patch/oracle/configurazionePdD/2019-11-08-patch.sql
@@ -1,1 +1,3 @@
 UPDATE porte_applicative SET autenticazione='none' WHERE (autenticazione='' OR autenticazione is null) AND tipo_servizio IN ('spc','test','url','wsdl','ldap','uddi','ebXMLRegistry');
+
+delete from users_stati ;
