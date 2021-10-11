64e191cc946c5ff8413700e759d240b583cc04f6
Andrea Poli <andreapoli80@gmail.com>
2021-10-11 17:25:47 +0200

    Predisposta versione 3.3.5

--- protocolli/trasparente/testsuite/karate/src/org/openspcoop2/core/protocolli/trasparente/testsuite/token/attribute_authority/JWTUtilities.java
+++ protocolli/trasparente/testsuite/karate/src/org/openspcoop2/core/protocolli/trasparente/testsuite/token/attribute_authority/JWTUtilities.java
@@ -1,3 +1,23 @@
+/*
+ * GovWay - A customizable API Gateway 
+ * https://govway.org
+ * 
+ * Copyright (c) 2005-2021 Link.it srl (https://link.it). 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.openspcoop2.core.protocolli.trasparente.testsuite.token.attribute_authority;
 
 import java.io.File;
@@ -8,6 +28,13 @@ import org.openspcoop2.utils.security.JOSESerialization;
 import org.openspcoop2.utils.security.JWSOptions;
 import org.openspcoop2.utils.security.JsonSignature;
 
+/**
+* JWTUtilities
+*
+* @author Francesco Scarlato (scarlato@link.it)
+* @author $Author$
+* @version $Rev$, $Date$
+*/
 public class JWTUtilities {
 
 	public static String builtJWT_OIDC(String sub,String iss,String username, String clientId,String audience) throws Exception {
