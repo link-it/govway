2778dc8d5e41a528d212060b63770f896bb22161
Andrea Poli <andreapoli80@gmail.com>
2021-10-20 08:37:20 +0200

    [ApiRsConfigurazione] Corrette intestazioni GPL e JavaDoc nelle classi org/openspcoop2/core/config/rs/server/api/impl/erogazioni/Connettore*.java

--- tools/rs/config/server/src/org/openspcoop2/core/config/rs/server/api/impl/erogazioni/ConnettoreNullApiHelper.java
+++ tools/rs/config/server/src/org/openspcoop2/core/config/rs/server/api/impl/erogazioni/ConnettoreNullApiHelper.java
@@ -1,3 +1,22 @@
+/*
+ * GovWay - A customizable API Gateway 
+ * https://govway.org
+ * 
+ * Copyright (c) 2005-2021 Link.it srl (https://link.it).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package org.openspcoop2.core.config.rs.server.api.impl.erogazioni;
 
 import java.util.Map;
@@ -13,6 +32,13 @@ import org.openspcoop2.core.registry.Connettore;
 import org.openspcoop2.web.ctrlstat.servlet.connettori.ConnettoriCostanti;
 import org.openspcoop2.web.lib.mvc.ServletUtils;
 
+/**
+ * ConnettoreNullApiHelper
+ * 
+ * @author $Author$
+ * @version $Rev$, $Date$
+ * 
+ */
 public class ConnettoreNullApiHelper extends AbstractConnettoreApiHelper<ConnettoreNull> {
 
 	@Override
