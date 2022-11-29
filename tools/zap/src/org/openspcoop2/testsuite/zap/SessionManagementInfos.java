e20fa78dc9f76a5fad8a704366b71019aa221699
Andrea Poli <andreapoli80@gmail.com>
2022-11-29 18:45:25 +0100

    Aggiunto file: tools/zap/src/org/openspcoop2/testsuite/zap/SessionManagementInfos.java

---
+++ tools/zap/src/org/openspcoop2/testsuite/zap/SessionManagementInfos.java
@@ -0,0 +1,66 @@
+/*
+ * GovWay - A customizable API Gateway 
+ * https://govway.org
+ * 
+ * Copyright (c) 2005-2022 Link.it srl (https://link.it).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.openspcoop2.testsuite.zap;
+
+import org.zaproxy.clientapi.core.ApiResponse;
+import org.zaproxy.clientapi.core.ApiResponseElement;
+import org.zaproxy.clientapi.core.ApiResponseList;
+import org.zaproxy.clientapi.core.ApiResponseSet;
+
+/**
+ * OpenAPI
+ *
+ * @author Andrea Poli (apoli@link.it)
+ * @author $Author$
+ * @version $Rev$, $Date$
+ */
+public class SessionManagementInfos {
+
+	public static void main(String[] args) throws Exception {
+				
+		ZAPContext context = new ZAPContext(args, SessionManagementInfos.class.getName(), "");
+		
+		System.out.println("=======================");
+		ApiResponse response = context.getClientApi().sessionManagement.getSupportedSessionManagementMethods();
+		ApiResponseList list = ((ApiResponseList) response);
+		System.out.println("Methods: "+list.getItems().size());
+		for (ApiResponse res : list.getItems()) {
+			ApiResponseElement re = (ApiResponseElement) res;
+			System.out.println("\n\t '"+re.getValue()+"'");
+			
+			ApiResponse responseParams = context.getClientApi().sessionManagement.getSessionManagementMethodConfigParams(re.getValue());
+			ApiResponseList responseParamsList = ((ApiResponseList) responseParams);
+			System.out.println("\tparams: "+responseParamsList.getItems().size());
+			int index = 1;
+			for (ApiResponse resParam : responseParamsList.getItems()) {
+				ApiResponseSet reParam = (ApiResponseSet) resParam;
+				System.out.println("\tparam "+index);
+				for (String key : reParam.getKeys()) {
+					System.out.println("\t\t'"+key+"'='"+reParam.getValue(key)+"'");
+				}
+				index++;
+			}
+		}
+		System.out.println("=======================\n\n");
+		
+		
+	}
+	
+}
