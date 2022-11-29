a261188eca899595f7e43ee9978af4df2e6be169
Andrea Poli <andreapoli80@gmail.com>
2022-11-29 19:56:24 +0100

    Predisposto rilascio 3.3.9.p2

--- resources/doc/src/releaseNotes/conf.py
+++ resources/doc/src/releaseNotes/conf.py
@@ -24,9 +24,9 @@ copyright = u'Copyright (c) 2005-2022 Link.it srl (https://link.it).'
 author = u'Link.it'
 
 # The short X.Y version
-version = u'3.3.9.p1'
+version = u'3.3.9.p2'
 # The full version, including alpha/beta/rc tags
-release = u'3.3.9.p1'
+release = u'3.3.9.p2'
 
 
 # -- General configuration ---------------------------------------------------
