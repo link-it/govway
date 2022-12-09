35e94c10ce3278944f4b6677a0ba8a1fdc9d7a0b
Andrea Poli <andreapoli80@gmail.com>
2022-12-09 17:16:21 +0100

    Predisposto rilascio versione 3.3.9.p3

--- resources/doc/src/manuali/conf.py
+++ resources/doc/src/manuali/conf.py
@@ -13,9 +13,9 @@ author = 'Link.it'
 numfig = True
 
 # The short X.Y version
-version = '3.3.9.p2'
+version = '3.3.9.p3'
 # The full version, including alpha/beta/rc tags
-release = '3.3.9.p2'
+release = '3.3.9.p3'
 
 
 # -- General configuration ---------------------------------------------------
