8a411e72fb38a65d36531328aa0d16778c87e3f9
Andrea Poli <andreapoli80@gmail.com>
2023-04-15 09:07:46 +0200

    [TestSuite] Corretto errore emerso dai test jenkins

--- testsuite/src/org/openspcoop2/testsuite/core/Repository.java
+++ testsuite/src/org/openspcoop2/testsuite/core/Repository.java
@@ -25,6 +25,8 @@ package org.openspcoop2.testsuite.core;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.openspcoop2.utils.Semaphore;
+
 /**
  * Repository degli id utilizzati nelle istanze di profili non asincroni.
  * 
@@ -36,32 +38,52 @@ import java.util.List;
 public class Repository {
 	private List<String> vet;
 	private int index;
+	private org.openspcoop2.utils.Semaphore semaphore = new Semaphore("Repository");
 
 	public Repository(){
 		this.vet=new ArrayList<>();
 		this.index=0;
 	}
 
-	public synchronized int add(String id){
-		this.vet.add(id);
-		return this.vet.indexOf(id);
+	public int add(String id){
+		this.semaphore.acquireThrowRuntime("add");
+		try {
+			this.vet.add(id);
+			//System.out.println("ADD (size:"+this.vet.size()+") indexOf("+this.vet.indexOf(id)+") index("+this.index+")");
+			return this.vet.indexOf(id);
+		}finally {
+			this.semaphore.release("add");
+		}
 	}
 
 
-	public synchronized String getNext(){
+	public String getNext(){
+		this.semaphore.acquireThrowRuntime("getNext");
 		String str;
 		try{
+			//System.out.println("GET NEXT (size:"+this.vet.size()+") index("+this.index+") ...");
 			str= this.vet.get(this.index);
 			this.index++;
+			//System.out.println("GET NEXT (size:"+this.vet.size()+") index("+this.index+")");
 		}
-		catch(ArrayIndexOutOfBoundsException nil){
+		catch(IndexOutOfBoundsException nil){
+			//System.out.println("GET NEXT (size:"+this.vet.size()+") index("+this.index+") ERROR OUT OF BOUND");
 			return null;
+		}
+		finally {
+			this.semaphore.release("add");
 		}
 		return str;
 	}
 	
-	public synchronized void setIndex(int i){
-		this.index = i;
+	public void setIndex(int i){
+		this.semaphore.acquireThrowRuntime("setIndex");
+		try {
+			//System.out.println("SET INDEX (size:"+this.vet.size()+") index("+i+") ...");
+			this.index = i;
+		}finally {
+			this.semaphore.release("setIndex");
+		}
 	}
 
 }
