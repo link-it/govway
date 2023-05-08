2f337daceb1ff55dd311c13c6a0bc2f7c56a5d23
Andrea Poli <andreapoli80@gmail.com>
2023-05-08 12:20:28 +0200

    [GovWayCore] Aggiunto file core/src/org/openspcoop2/pdd/core/token/TokenCacheItem.java

---
+++ core/src/org/openspcoop2/pdd/core/token/TokenCacheItem.java
@@ -0,0 +1,97 @@
+/*
+ * GovWay - A customizable API Gateway 
+ * https://govway.org
+ * 
+ * Copyright (c) 2005-2023 Link.it srl (https://link.it). 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.openspcoop2.pdd.core.token;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * TokenCacheItem
+ *
+ * @author Poli Andrea (apoli@link.it)
+ * @author $Author$
+ * @version $Rev$, $Date$
+ */
+public class TokenCacheItem implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	private String idTransazione;
+	
+	private String token;
+	
+	private Date exp;
+	
+	private String digestAlgorithm;
+	private String digest;
+	
+	private boolean inCache;
+
+	public TokenCacheItem(String idTransazione) {
+		this.idTransazione = idTransazione;
+	}
+	
+	public String getToken() {
+		return this.token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String getDigestAlgorithm() {
+		return this.digestAlgorithm;
+	}
+
+	public void setDigestAlgorithm(String digestAlgorithm) {
+		this.digestAlgorithm = digestAlgorithm;
+	}
+
+	public String getDigest() {
+		return this.digest;
+	}
+
+	public void setDigest(String digest) {
+		this.digest = digest;
+	}
+
+	public boolean isInCache() {
+		return this.inCache;
+	}
+
+	public void setInCache(boolean inCache) {
+		this.inCache = inCache;
+	}
+	
+	public Date getExp() {
+		return this.exp;
+	}
+
+	public void setExp(Date exp) {
+		this.exp = exp;
+	}
+
+	public String getIdTransazione() {
+		return this.idTransazione;
+	}
+	public void setIdTransazione(String idTransazione) {
+		this.idTransazione = idTransazione;
+	}
+}
