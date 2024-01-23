2913decc24cd1334b9c55ffdbce77828e2fb36fd
Andrea Poli <andreapoli80@gmail.com>
2024-01-23 18:33:54 +0100

    [Documentazione] Iniziata predisposizione rilascio della versione 3.3.14

--- resources/doc/src/releaseNotes/conf.py
+++ resources/doc/src/releaseNotes/conf.py
@@ -24,9 +24,9 @@ copyright = u'Copyright (c) 2005-2024 Link.it srl (https://link.it).'
 author = u'Link.it'
 
 # The short X.Y version
-version = u'3.3.13.p1'
+version = u'3.3.14'
 # The full version, including alpha/beta/rc tags
-release = u'3.3.13.p1'
+release = u'3.3.14'
 
 
 # -- General configuration ---------------------------------------------------
