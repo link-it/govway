078d09020f71377b220bbf69c0301c8e39a1eadf
Andrea Poli <andreapoli80@gmail.com>
2024-01-25 17:33:23 +0100

    [Documentazione] Deprecata funzionalità di generazione dell'interfaccia wsdl

--- core/deploy/sql/patch/sqlserver/registroServizi/2023-11-10-patch.sql
+++ core/deploy/sql/patch/sqlserver/registroServizi/2023-11-10-patch.sql
@@ -21,7 +21,7 @@ ALTER TABLE servizi_fruitori ADD utente_richiedente VARCHAR(255);
 ALTER TABLE servizi_fruitori ADD data_creazione DATETIME2;
 ALTER TABLE servizi_fruitori ADD utente_ultima_modifica VARCHAR(255);
 ALTER TABLE servizi_fruitori ADD data_ultima_modifica DATETIME2;
-UPDATE servizi_fruitori sf SET utente_richiedente=(select superuser from servizi s where s.id=sf.id_servizio);
+UPDATE servizi_fruitori SET utente_richiedente=(select superuser from servizi s where s.id=servizi_fruitori.id_servizio);
 UPDATE servizi_fruitori SET data_creazione=ora_registrazione;
 
 
