0a4d2234c72d9a0690b18928aa2e98144914d7a9
Andrea Poli <andreapoli80@gmail.com>
2024-05-27 13:00:05 +0200

    Aggiunta classe EnvironmentJavaPropertiesReader

---
+++ core/src/org/openspcoop2/pdd/core/dynamic/EnvironmentJavaPropertiesReader.java
@@ -0,0 +1,48 @@
+/*
+ * GovWay - A customizable API Gateway 
+ * https://govway.org
+ * 
+ * Copyright (c) 2005-2024 Link.it srl (https://link.it). 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.openspcoop2.pdd.core.dynamic;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+
+/**
+ * EnvironmentJavaPropertiesReader
+ *
+ * @author Andrea Poli (apoli@link.it)
+ * @author $Author$
+ * @version $Rev$, $Date$
+ */
+public class EnvironmentJavaPropertiesReader extends PropertiesReader {
+	
+	public EnvironmentJavaPropertiesReader(Logger log) {
+		super(log);
+	}
+		
+	@Override
+	public String read(String nome) throws DynamicException{
+		String envV = System.getenv(nome);
+		if(envV!=null && StringUtils.isNotEmpty(envV)) {
+			return envV;
+		}
+		return System.getProperty(nome);
+	}
+	
+}
