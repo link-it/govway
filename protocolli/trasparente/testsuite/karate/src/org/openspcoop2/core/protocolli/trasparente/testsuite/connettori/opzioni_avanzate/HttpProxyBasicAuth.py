228cc7c6e78f0610b88ffe1da0687dffb78935a4
Andrea Poli <andreapoli80@gmail.com>
2024-10-15 16:00:35 +0200

    [TestSuite] Corretti test che verificano la funzionalità http-proxy di un connettore

--- protocolli/trasparente/testsuite/karate/src/org/openspcoop2/core/protocolli/trasparente/testsuite/connettori/opzioni_avanzate/HttpProxyBasicAuth.py
+++ protocolli/trasparente/testsuite/karate/src/org/openspcoop2/core/protocolli/trasparente/testsuite/connettori/opzioni_avanzate/HttpProxyBasicAuth.py
@@ -6,13 +6,23 @@ USERNAME = "UTENTEGOVWAY"
 PASSWORD = "PASSWORDGOVWAY"
 AUTH_HEADER = f"Basic {base64.b64encode(f'{USERNAME}:{PASSWORD}'.encode()).decode()}"
 
+# Determina quale classe usare per creare la risposta (dipende dalla versione di mimproxy installata)
+try:
+    # Tentativo di utilizzare HTTPResponse (per versioni più recenti)
+    from mitmproxy.http import HTTPResponse
+    ResponseClass = HTTPResponse
+except ImportError:
+    # Fallback a Response (per versioni più vecchie)
+    from mitmproxy.http import Response
+    ResponseClass = Response
+
 def request(flow: http.HTTPFlow) -> None:
     # Controlla se l'header Authorization è presente e se corrisponde alle credenziali
     auth_header = flow.request.headers.get("Proxy-Authorization")
     
     if auth_header != AUTH_HEADER:
         # Se non corrisponde, restituisci una risposta di errore
-        flow.response = http.Response.make(
+        flow.response = ResponseClass.make(
             407,  # Codice di errore per autenticazione richiesta
             b"",
             {
