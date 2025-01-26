ec2d78f93c567248bbf7095bf757f3bec273b68a
Andrea Poli <andreapoli80@gmail.com>
2025-01-26 12:37:23 +0100

    [GovWayCore] Iniziata predisposizione per rilascio v3.3.16

--- resources/doc/src/manuali/conf.py
+++ resources/doc/src/manuali/conf.py
@@ -13,9 +13,9 @@ author = 'Link.it'
 numfig = True
 
 # The short X.Y version
-version = '3.3.15.p2'
+version = '3.3.16'
 # The full version, including alpha/beta/rc tags
-release = '3.3.15.p2'
+release = '3.3.16'
 
 
 # -- General configuration ---------------------------------------------------
--- resources/doc/src/releaseNotes/conf.py
+++ resources/doc/src/releaseNotes/conf.py
@@ -24,9 +24,9 @@ copyright = u'Copyright (c) 2005-2025 Link.it srl (https://link.it).'
 author = u'Link.it'
 
 # The short X.Y version
-version = u'3.3.15.p2'
+version = u'3.3.16'
 # The full version, including alpha/beta/rc tags
-release = u'3.3.15.p2'
+release = u'3.3.16'
 
 
 # -- General configuration ---------------------------------------------------
