fb49fff0bc97f2cd9fd4d11c3b07c01e4bcc67d1
Andrea Poli <andreapoli80@gmail.com>
2025-07-09 15:57:39 +0200

    [InstallerVersioneBinaria] Corrette patch riguardanti la tabella 'statistiche' e 'transazioni_info' per postgresql e hsql; non veniva aggiunta l'autovalorizzazione della colonna id.

--- core/deploy/sql/patch/postgresql/archiviComunicazioni/2025-02-17-patch.sql
+++ core/deploy/sql/patch/postgresql/archiviComunicazioni/2025-02-17-patch.sql
@@ -3,6 +3,7 @@ CREATE SEQUENCE seq_statistiche start 1 increment 1 maxvalue 9223372036854775807
 ALTER TABLE statistiche ADD COLUMN id BIGINT;
 UPDATE statistiche SET id = nextval('seq_statistiche');
 ALTER TABLE statistiche ALTER COLUMN id SET NOT NULL;
+ALTER TABLE statistiche ALTER COLUMN id SET DEFAULT nextval('seq_statistiche');
 ALTER TABLE statistiche ADD CONSTRAINT pk_statistiche PRIMARY KEY (id);
 
 CREATE SEQUENCE seq_transazioni_info start 1 increment 1 maxvalue 9223372036854775807 minvalue 1 cache 1 CYCLE;
@@ -10,4 +11,5 @@ CREATE SEQUENCE seq_transazioni_info start 1 increment 1 maxvalue 92233720368547
 ALTER TABLE transazioni_info ADD COLUMN id BIGINT;
 UPDATE transazioni_info SET id = nextval('seq_transazioni_info');
 ALTER TABLE transazioni_info ALTER COLUMN id SET NOT NULL;
+ALTER TABLE transazioni_info ALTER COLUMN id SET DEFAULT nextval('seq_transazioni_info');
 ALTER TABLE transazioni_info ADD CONSTRAINT pk_transazioni_info PRIMARY KEY (id);
