ec624798cc20d7ce8de49ffad71dd10c286c8c01
Andrea Poli <andreapoli80@gmail.com>
2025-10-28 18:56:59 +0100

    [GovWayCore] Verificate licenze GPL nelle intestazioni dei file java

--- tools/web_interfaces/monitor/src/src_core/org/openspcoop2/web/monitor/core/servlet/OAuth2UserServlet.java
+++ tools/web_interfaces/monitor/src/src_core/org/openspcoop2/web/monitor/core/servlet/OAuth2UserServlet.java
@@ -1,3 +1,22 @@
+/*
+ * GovWay - A customizable API Gateway
+ * https://govway.org
+ *
+ * Copyright (c) 2005-2025 Link.it srl (https://link.it).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package org.openspcoop2.web.monitor.core.servlet;
 
 import java.io.IOException;
@@ -28,6 +47,12 @@ import org.openspcoop2.web.monitor.core.logger.LoggerManager;
 import org.openspcoop2.web.monitor.core.utils.SessionUtils;
 import org.slf4j.Logger;
 
+/**
+ * OAuth2UserServlet
+ *
+ * @author $Author$
+ * @version $Rev$, $Date$
+ */
 public class OAuth2UserServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
