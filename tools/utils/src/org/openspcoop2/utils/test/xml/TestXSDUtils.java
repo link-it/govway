b2dc6f57f862c00966eafc2e80bbd9b936e5bd7a
Andrea Poli <andreapoli80@gmail.com>
2025-11-17 17:15:09 +0100

    Aggiunti file mancanti TestXSDUtils

---
+++ tools/utils/src/org/openspcoop2/utils/test/xml/TestXSDUtils.java
@@ -0,0 +1,47 @@
+/*
+ * GovWay - A customizable API Gateway 
+ * https://govway.org
+ * 
+ * Copyright (c) 2005-2025 Link.it srl (https://link.it).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.openspcoop2.utils.test.xml;
+
+import org.openspcoop2.utils.test.Costanti;
+import org.openspcoop2.utils.test.TestLogger;
+import org.testng.annotations.Test;
+
+/**
+ * TestXSDUtils
+ * 
+ * @author Tommaso Burlon (tommaso.burlon@link.it)
+ * @author $Author$
+ * @version $Rev$, $Date$
+ */
+public class TestXSDUtils {
+
+	private static final String ID_TEST = "XML-XSDUtils";
+	
+	@Test(groups={Costanti.GRUPPO_UTILS,Costanti.GRUPPO_UTILS+"."+ID_TEST})
+	public void testCycle() throws Exception {
+		org.openspcoop2.utils.xml.test.TestXSDUtils testsuite = new org.openspcoop2.utils.xml.test.TestXSDUtils();
+		
+		TestLogger.info("Run test '"+ID_TEST+"' ...");
+		testsuite.testCycle();
+		TestLogger.info("Run test '"+ID_TEST+"' ok");
+		
+	}
+	
+}
